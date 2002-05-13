c0d48814d66d49336b92723af0ed098f893f18d4
Ben Liblit <liblit@cs.wisc.edu>
2002-05-13 21:28:11 +0000

    Tighten up construction of new instructions so that we don't walk lists more than once.

--- splitAfterCalls.ml
+++ splitAfterCalls.ml
@@ -1,25 +1,6 @@
 open Cil
 
-(* look into Cil.compactStmts *)
-
-let rec slurp = function
-  | [] ->
-      ([], [])
-  | Call _ as call :: tail ->
-      ([call], tail)
-  | other :: tail ->
-      let more, remainder = slurp tail in
-      (other :: more, remainder)
-
-	
-let rec split instrs =
-  let initial, remainder = slurp instrs in
-  if remainder == [] then
-    [initial]
-  else
-    initial :: split remainder
-
-
+  
 class visitor = object
   inherit nopCilVisitor
       
@@ -27,19 +8,29 @@ class visitor = object
     match stmt.skind with
     | Instr(instrs) ->
 	begin
+	  let rec split = function
+	    | [] -> []
+	    | instrs ->
+		let rec slurp = function
+		  | [] ->
+		      ([], [])
+		  | Call _ as call :: tail ->
+		      ([call], tail)
+		  | other :: tail ->
+		      let more, remainder = slurp tail in
+		      (other :: more, remainder)
+		in
+		
+		let (initial, remainder) = slurp instrs in
+		mkStmt(Instr(initial)) :: split remainder
+	  in
+	  
 	  match split instrs with
 	  | [] -> DoChildren
 	  | [_] -> DoChildren
 	  | splits ->
-	      ChangeTo {stmt with skind =
-			Block (mkBlock (List.map
-					  (fun split -> mkStmt (Instr split))
-					  splits))}
+	      ChangeTo {stmt with skind = Block (mkBlock splits)}
 	end
     | _ -> DoChildren
 	  
 end
-    
-;;
-
-ignore(TestHarness.main new visitor)

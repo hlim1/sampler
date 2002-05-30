76b656376ec6447f59653b2d60e8930cdc0761db
Ben Liblit <liblit@cs.wisc.edu>
2002-05-30 04:32:14 +0000

    Very simplistic code for duplicating a function's body, with an "if" selecting between them.  We generate new labels for the duplicate, but everything else is shared.

---
+++ duplicate.ml
@@ -0,0 +1,30 @@
+open Cil
+open Identity
+
+
+let cloneLabel = function
+  | Label (name, location, _) ->
+      Label (name ^ "__dup", location, false)
+  | other -> other
+	
+	
+class visitor = object(self)
+  inherit Instrument.visitor
+
+  method vfunc func =
+    let predicate = one in
+    let original = func.sbody in
+    let clone = visitCilBlock (self :> cilVisitor) original in
+    let choice = mkStmt (If (predicate, original, clone, func.svar.vdecl)) in
+    func.sbody <- mkBlock [choice];
+    SkipChildren
+
+  method vstmt stmt =
+    let originals = stmt.labels in
+    let replacements = List.map cloneLabel originals in
+    ChangeTo { stmt with labels = replacements }
+end
+
+
+let phase _ =
+  ("Duplicate", visitCilFileSameGlobals new visitor)

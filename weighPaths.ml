bd2fba39cd6c81bbb50035a64b4422a065fc5135
Ben Liblit <liblit@cs.wisc.edu>
2002-06-05 23:25:12 +0000

    Rework weight computation to handle headers represented as single nodes (e.g. backward gotos) rather than edges.

--- weighPaths.ml
+++ weighPaths.ml
@@ -2,12 +2,15 @@ open Cil
 open Printf
 
 
-let rec weighPaths headers =
+type weightsMap = int StmtMap.container
+
+
+let rec weigh headers =
   
   let cache = new StmtMap.container in
 
-  let rec subweight node succ =
-    if headers#mem (node, succ) then
+  let rec subweight succ =
+    if List.mem succ headers then
       0
     else
       weight succ
@@ -16,7 +19,7 @@ let rec weighPaths headers =
     try cache#find node with
       Not_found ->
 	let myWeight = Stores.count_stmt node in
-	let childWeights = List.map (subweight node) node.succs in
+	let childWeights = List.map subweight node.succs in
 	let maxChildWeight = List.fold_left max 0 childWeights in
 	let total = myWeight + maxChildWeight in
 	cache#add node total;
@@ -25,37 +28,10 @@ let rec weighPaths headers =
 
   let weights = new StmtMap.container in
 
-  headers#iter begin
-    fun (s, destination) ->
-      weights#add destination (weight destination)
-  end;
-
+  let compute header =
+    weights#add header (weight header)
+  in
+  
+  eprintf "weighing %i headers\n" (List.length headers);
+  List.iter compute headers;
   weights
-    
-    
-(**********************************************************************)
-	  
-	  
-class visitor = object
-  inherit FunctionBodyVisitor.visitor
-      
-  method vfunc func =
-    printf "  weighing %s()" func.svar.vname;
-    print_newline ();
-    
-    let cfg = Cfg.cfg func in
-    let headers = CollectHeaders.collectHeaders cfg in
-    printf "    collected %i header edges\n" headers#size;
-    let weights = weighPaths headers
-    and nonzero = ref 0 in
-    weights#iter (fun _ weight -> if weight != 0 then incr nonzero);
-    printf "    weighed %i regions; %i nonzero\n" weights#size !nonzero;
-
-    weights#iter begin
-      fun node weight ->
-	printf "    weight below %i: %i" node.sid weight;
-	print_newline ()
-    end;
-    
-    SkipChildren
-end

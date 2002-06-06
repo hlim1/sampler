1b2a0e2468be369822ecbad69c8c0dc8ad456bf1
Ben Liblit <liblit@cs.wisc.edu>
2002-06-06 02:50:58 +0000

    A simple visitor/phase which just prepares for CFG computation.

---
+++ prepareCfg.ml
@@ -0,0 +1,15 @@
+open Cil
+
+
+class visitor = object
+  inherit FunctionBodyVisitor.visitor
+
+  method vfunc func =
+    prepareCFG func;
+    SkipChildren
+end
+
+
+let phase =
+  "PrepareCfg",
+  visitCilFileSameGlobals new visitor

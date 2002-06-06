267bbeac21307a10d2384ec434e5a92532fdccab
Ben Liblit <liblit@cs.wisc.edu>
2002-06-06 04:02:24 +0000

    Visit children of a statement.  If the statement is, for example, an If, then its block children may contain more statements.

--- splitAfterCalls.ml
+++ splitAfterCalls.ml
@@ -5,33 +5,35 @@ class visitor = object
   inherit FunctionBodyVisitor.visitor
 
   method vstmt stmt =
-    match stmt.skind with
-    | Instr(instrs) ->
-	let rec split = function
-	  | [] -> []
-	  | instrs ->
-	      let rec slurp = function
-		| [] ->
-		    ([], [])
-		| Call _ as call :: tail ->
-		    ([call], tail)
-		| other :: tail ->
-		    let more, remainder = slurp tail in
-		    (other :: more, remainder)
-	      in
-	      
-	      let (initial, remainder) = slurp instrs in
-	      mkStmt(Instr(initial)) :: split remainder
-	in
-	
-	begin
-	  match split instrs with
-	  | [] -> ()
-	  | [_] -> ()
-	  | splits -> stmt.skind <- Block (mkBlock splits)
-	end;
-	
-	SkipChildren
+    begin
+      match stmt.skind with
+      | Instr(instrs) ->
+	  let rec split = function
+	    | [] -> []
+	    | instrs ->
+		let rec slurp = function
+		  | [] ->
+		      ([], [])
+		  | Call _ as call :: tail ->
+		      ([call], tail)
+		  | other :: tail ->
+		      let more, remainder = slurp tail in
+		      (other :: more, remainder)
+		in
+		
+		let (initial, remainder) = slurp instrs in
+		mkStmt(Instr(initial)) :: split remainder
+	  in
 	  
-    | _ -> DoChildren
+	  begin
+	    match split instrs with
+	    | [] -> ()
+	    | [_] -> ()
+	    | splits -> stmt.skind <- Block (mkBlock splits)
+	  end
+	    
+      | _ -> ()
+    end;
+    
+    DoChildren
 end

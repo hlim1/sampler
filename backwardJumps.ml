7db02d2568ef8ec55129d04417f9e8d3d31a0c1c
Ben Liblit <liblit@cs.wisc.edu>
2002-06-07 21:48:42 +0000

    Let backward-jump choice points evaporate away to nothingness if the acyclic region that follows has no events of interest.

--- backwardJumps.ml
+++ backwardJumps.ml
@@ -2,20 +2,22 @@ open Cil
 
 
 let patch clones weights =
+  
   let patchOne jump =
-    match jump.skind with
-    | Goto (destination, location) ->
+    let weight = weights#find jump in
+    if weight != 0 then
+      match jump.skind with
+      | Goto (destination, location) ->
 
-	let clonedDest = clones#find !destination in
-	let gotoStandard = mkBlock [mkStmt jump.skind] in
-	let gotoInstrumented = mkBlock [mkStmt (Goto (ref clonedDest, location))] in
-	
-	let weight = weights#find jump in
-	let choice = LogIsImminent.choose location weight gotoInstrumented gotoStandard in
-	
-	jump.skind <- choice
-	    
-    | _ -> failwith "unexpected statement kind in backward jumps list"
+	  let clonedDest = clones#find !destination in
+	  let gotoStandard = mkBlock [mkStmt jump.skind] in
+	  let gotoInstrumented = mkBlock [mkStmt (Goto (ref clonedDest, location))] in
+	  
+	  let choice = LogIsImminent.choose location weight gotoInstrumented gotoStandard in
+	  
+	  jump.skind <- choice
+	      
+      | _ -> failwith "unexpected statement kind in backward jumps list"
   in
   
   List.iter patchOne

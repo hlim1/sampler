00de311953c51257deaaae3fbcc7d0a4234bb21e
Ben Liblit <liblit@cs.wisc.edu>
2002-07-01 02:50:51 +0000

    Basic definitions, especially of file suffixes.

---
+++ defs.mk
@@ -0,0 +1,17 @@
+include $(top)/config.mk
+
+ifdef profile
+native := 1
+ocamlflags := -p
+endif
+
+ifdef native
+ocamlc := ocamlopt.opt
+cma := cmxa
+cmo := cmx
+else
+ocamlflags := -g
+ocamlc := ocamlc.opt
+cma := cma
+cmo := cmo
+endif

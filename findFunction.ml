b0d3c5d9612860d32a364c3598175903253541a1
Ben Liblit <liblit@cs.wisc.edu>
2002-07-02 21:41:32 +0000

    Avoid shadowing a name when what I really wanted to do was compare two things for equality.

--- findFunction.ml
+++ findFunction.ml
@@ -3,7 +3,7 @@ open Cil
 
 let find name =
   let predicate = function
-    | {vname = name; vtype = TFun _} -> true
+    | {vname = vname; vtype = TFun _} when vname = name -> true
     | _ -> false
   in
   FindGlobal.find predicate

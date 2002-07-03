3f4b3e1ff36f1c1ca552c651f39b91b94bc79a5d
Ben Liblit <liblit@cs.wisc.edu>
2002-07-03 04:57:46 +0000

    Avoid queuing statements, as it doesn't seem to actually work.  I get warnings about losing queued statements.  Instead, just build a new block and inject the statements ourself.

--- insertSkipsBefore.ml
+++ insertSkipsBefore.ml
@@ -4,7 +4,12 @@ open Cil
 class visitor sites skipLog = object (self)
   inherit InsertSkipsVisitor.visitor sites skipLog
       
-  method insertSkip skip _ =
-    self#queueInstr [skip];    
-    DoChildren
+  method insertSkip skip statement =
+    let statements = [mkStmtOneInstr skip; mkStmt statement.skind] in
+    let block = Block (mkBlock statements) in
+    let replace stmt =
+      stmt.skind <- block;
+      stmt
+    in
+    ChangeDoChildrenPost (statement, replace)
 end
--- loads/logs.ml
+++ loads/logs.ml
@@ -3,25 +3,28 @@ open Pretty
 
 
 class visitor logger (sites : FindSites.map) = object (self)
-    inherit FunctionBodyVisitor.visitor
+  inherit FunctionBodyVisitor.visitor
 
-    method vstmt statement =
-      begin
-	try
-	  let outputs = sites#find statement in
-	  let formats, arguments = List.split (OutputSet.OutputSet.elements outputs) in
-	  let format = ("%s:%u:\n" ^ String.concat "" formats) in
-	  let location = Where.statement statement in
-	  let call =
-	    Call (None, logger,
-		  mkString format
-		  :: mkString location.file
-		  :: kinteger IUInt location.line
-		  :: arguments,
-		  location)
-	  in
-	  self#queueInstr [call]
-	with Not_found -> ()
-      end;
-      DoChildren
-  end
+  method vstmt statement =
+    try
+      let outputs = sites#find statement in
+      let formats, arguments = List.split (OutputSet.OutputSet.elements outputs) in
+      let format = ("%s:%u:\n" ^ String.concat "" formats) in
+      let location = Where.statement statement in
+      let call =
+	Call (None, logger,
+	      mkString format
+	      :: mkString location.file
+	      :: kinteger IUInt location.line
+	      :: arguments,
+	      location)
+      in
+      let block = Block (mkBlock [mkStmtOneInstr call; mkStmt statement.skind]) in
+      let replace stmt =
+	stmt.skind <- block;
+	stmt
+      in
+      ChangeDoChildrenPost (statement, replace)
+	
+    with Not_found -> DoChildren
+end

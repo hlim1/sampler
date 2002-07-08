504af341bb323df111fa2ad1b88c79d7c96649b9
Ben Liblit <liblit@cs.wisc.edu>
2002-07-08 10:59:52 +0000

    Name fiddling for readability.

--- functionEntry.ml
+++ functionEntry.ml
@@ -13,16 +13,17 @@ let patch func weights countdown instrumented =
   let weight = weights#find entry in
 
   let body =
+    let original = func.sbody in
     if weight == 0 then
       let finis = mkEmptyStmt () in
       finis.labels <- [ Label ("finis", locUnknown, false) ];
       [ import;
-	mkStmt (Block func.sbody);
+	mkStmt (Block original);
 	mkStmt (Goto (ref finis, locUnknown));
 	mkStmt (Block instrumented);
 	finis ]
     else
-      let choice = countdown#choose locUnknown weight instrumented func.sbody in
+      let choice = countdown#choose locUnknown weight instrumented original in
       [ import;
 	mkStmt choice ]
   in

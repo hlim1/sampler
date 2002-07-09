8f0dbf4bda3a5bb1abc0859c9358eed655dc5caa
Ben Liblit <liblit@cs.wisc.edu>
2002-07-09 02:24:15 +0000

    Don't instrument functions that carry the "no_instrument_function" attribute.  This is the same attribute checked by gcc when inserting profiling instrumentation.

--- transformVisitor.ml
+++ transformVisitor.ml
@@ -16,46 +16,49 @@ class virtual visitor file = object(self)
   method virtual placeInstrumentation : stmt -> stmt list -> stmt list
 
   method vfunc func =
-    prepareCFG func;
-    RemoveLoops.visit func;
-    IsolateInstructions.visit func;
-    let afterCalls = Calls.prepatch func in
-    let cfg = computeCFGInfo func false in
-    let sites = Sites.collect (self#collectOutputs func) cfg in
-
-    if not sites#isEmpty then
+    if not (hasAttribute "no_instrument_function" func.svar.vattr) then
       begin
-	let forwardJumps, backwardJumps = ClassifyJumps.visit func in
-	let entry = FunctionEntry.find func in
-	let headers = entry :: backwardJumps @ afterCalls in
-	let weights = WeighPaths.weigh sites headers in
-	let countdown = new Countdown.countdown countdownToken func in
-	let original, instrumented, clones = Duplicate.duplicateBody func in
-	
-	ForwardJumps.patch clones forwardJumps;
-	BackwardJumps.patch clones weights countdown backwardJumps;
-	Calls.patch clones weights countdown afterCalls;
-	Calls.postpatch func countdown;
-	FunctionEntry.patch func weights countdown instrumented;
-	
-	let combine code instrumentation =
-	  let codeStmt = mkStmt code.skind in
-	  let combined = self#placeInstrumentation codeStmt instrumentation in
-	  code.skind <- Block (mkBlock combined)
-	in
-	
-	let instrument original outputs =
-	  let location = get_stmtLoc original.skind in
-	  let skip = countdown#decrement location in
-	  combine original [mkStmtOneInstr skip];
-	  
-	  let clone = ClonesMap.findCloneOf clones original in
-	  let instrumentation = logger location outputs in
-	  let conditional = countdown#log location instrumentation in
-	  combine clone conditional
-	in
-	
-	sites#iter instrument
+	prepareCFG func;
+	RemoveLoops.visit func;
+	IsolateInstructions.visit func;
+	let afterCalls = Calls.prepatch func in
+	let cfg = computeCFGInfo func false in
+	let sites = Sites.collect (self#collectOutputs func) cfg in
+
+	if not sites#isEmpty then
+	  begin
+	    let forwardJumps, backwardJumps = ClassifyJumps.visit func in
+	    let entry = FunctionEntry.find func in
+	    let headers = entry :: backwardJumps @ afterCalls in
+	    let weights = WeighPaths.weigh sites headers in
+	    let countdown = new Countdown.countdown countdownToken func in
+	    let original, instrumented, clones = Duplicate.duplicateBody func in
+	    
+	    ForwardJumps.patch clones forwardJumps;
+	    BackwardJumps.patch clones weights countdown backwardJumps;
+	    Calls.patch clones weights countdown afterCalls;
+	    Calls.postpatch func countdown;
+	    FunctionEntry.patch func weights countdown instrumented;
+	    
+	    let combine code instrumentation =
+	      let codeStmt = mkStmt code.skind in
+	      let combined = self#placeInstrumentation codeStmt instrumentation in
+	      code.skind <- Block (mkBlock combined)
+	    in
+	    
+	    let instrument original outputs =
+	      let location = get_stmtLoc original.skind in
+	      let skip = countdown#decrement location in
+	      combine original [mkStmtOneInstr skip];
+	      
+	      let clone = ClonesMap.findCloneOf clones original in
+	      let instrumentation = logger location outputs in
+	      let conditional = countdown#log location instrumentation in
+	      combine clone conditional
+	    in
+	    
+	    sites#iter instrument
+	  end
       end;
 
     SkipChildren

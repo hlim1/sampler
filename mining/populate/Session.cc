b52ac4e810564cf34d9389c49dde9902ec410855
Ben Liblit <liblit@cs.wisc.edu>
2002-08-02 06:53:22 +0000

    Use progress tracker to report on our progress.

--- mining/populate/Session.cc
+++ mining/populate/Session.cc
@@ -1,5 +1,6 @@
 #include <libpq++/pgdatabase.h>
 #include <sstream>
+#include "Progress.h"
 #include "Session.h"
 #include "require.h"
 
@@ -17,11 +18,15 @@ void Session::upload(PgDatabase &database, unsigned short signum) const
   const string sessionId = database.GetValue(0, 0);
 
   {
+    Progress progress("upload sites", size());
     const ExecStatusType status = database.Exec("COPY sites FROM STDIN");
     require(status == PGRES_COPY_IN, database);
     
     for (const_iterator site = begin(); site != end(); ++site)
-      site->copySite(database, sessionId);
+      {
+	site->copySite(database, sessionId);
+	progress.bump();
+      }
     
     database.PutLine("\\.\n");
     const int error = database.EndCopy();
@@ -29,12 +34,16 @@ void Session::upload(PgDatabase &database, unsigned short signum) const
   }
   
   {
+    Progress progress("upload sites' samples", size());
     const ExecStatusType status = database.Exec("COPY samples FROM STDIN");
     require(status == PGRES_COPY_IN, database);
     
     for (const_iterator site = begin(); site != end(); ++site)
-      site->copySamples(database, sessionId);
-
+      {
+	site->copySamples(database, sessionId);
+	progress.bump();
+      }
+    
     database.PutLine("\\.\n");
     const int error = database.EndCopy();
     require(!error, database);

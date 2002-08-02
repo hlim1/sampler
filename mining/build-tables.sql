d60d9eb4bc01677f38e8f80494dac9bec45abc12
Ben Liblit <liblit@cs.wisc.edu>
2002-08-02 06:55:18 +0000

    Terminology fixup.  The sequential numbers assigned within a session will be called "site" numbers, while a "sample" is a single expression and value within a site.

--- mining/build-tables.sql
+++ mining/build-tables.sql
@@ -9,21 +9,21 @@ CREATE TABLE sessions (
 
 CREATE TABLE sites (
   session int NOT NULL CHECK (session > 0) REFERENCES sessions MATCH FULL,
-  sample bigint NOT NULL CHECK (sample > 0),
+  site bigint NOT NULL CHECK (site > 0),
   file text NOT NULL CHECK (file <> ''),
   line int NOT NULL CHECK (line > 0),
 
-  UNIQUE (session, sample)
+  UNIQUE (session, site)
 );
 
 
 CREATE TABLE samples (
   session int NOT NULL CHECK (session > 0),
-  sample int NOT NULL CHECK (sample > 0),
+  site int NOT NULL CHECK (site > 0),
   expression text NOT NULL CHECK (expression <> ''),
   type smallint NOT NULL CHECK (type BETWEEN 1 AND 15),
   value text NOT NULL CHECK (value <> ''),
 
-  UNIQUE (session, sample, expression),
-  FOREIGN KEY (session, sample) REFERENCES sites (session, sample) MATCH FULL
+  UNIQUE (session, site, expression),
+  FOREIGN KEY (session, site) REFERENCES sites (session, site) MATCH FULL
 );

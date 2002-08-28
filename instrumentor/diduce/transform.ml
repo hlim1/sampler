ef6ef8f284f22d4a6ad7a6154eb9261a96e1acea
Ben Liblit <liblit@cs.wisc.edu>
2002-08-28 00:56:16 +0000

    DIDUCE-like tracking of function return values and formal parameter initial values.

---
+++ instrumentor/diduce/transform.ml
@@ -0,0 +1,36 @@
+open Cil
+open Mapconcat
+
+
+class visitor = object (self)
+  inherit SkipVisitor.visitor
+
+  method vglob global =
+    match global with
+    | GFun (fundec, _) ->
+
+	let trackers = ref [] in
+	let newTracker scope typ =
+	  let tracker = new Tracker.tracker fundec scope typ in
+	  trackers := tracker :: !trackers;
+	  tracker
+	in
+
+	Returns.transform fundec newTracker;
+	Formals.transform fundec newTracker;
+
+	if !trackers == [] then
+	  SkipChildren
+	else
+	  ChangeTo (mapconcat (fun tracker -> tracker#define) [global] !trackers)
+
+    | _ ->
+	SkipChildren
+end
+
+
+let phase =
+  "Transform",
+  fun file ->
+    let visitor = new visitor in
+    visitCilFile (visitor :> cilVisitor) file

de9d00df156db2b8df0a225f79ccf5258ddcd466
Ben Liblit <liblit@cs.wisc.edu>
2002-09-10 06:34:57 +0000

    Start a major rewrite to separate insertion of logging calls from randomized partial execution of those calls.  We want to be able to do the randomization transformation on other things, such as the CCured runtime checks.

---
+++ instrumentor/logging/localVars/log.ml
@@ -0,0 +1,12 @@
+open Cil
+
+
+class visitor file func =
+  object
+    inherit LogCollector.visitor file
+
+    val outputs = Collect.collect func
+
+    method private collectOutputs = FindOutputs.select outputs
+    method private placeInstrumentation code log = [log; code]
+  end

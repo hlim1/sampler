4481a4792fb1a0b79d6c358379dbd2d155cc22a2
Ben Liblit <liblit@cs.wisc.edu>
2002-09-12 07:45:43 +0000

    Add a hairy pattern to match the macro expansion of CHECK_RETURNPTR.

--- instrumentor/decure/find.ml
+++ instrumentor/decure/find.ml
@@ -5,29 +5,36 @@ open Str
 let checkPattern = regexp "^__CHECK_"
 
 
-let isCheck = function
-  | "__CHECK_STOREPTR"
-  | "__CHECK_FUNTIONPOINTER"
+type classification = Check | Fail | Generic
+
+let classify = function
+  | "__CHECK_BOUNDS_LEN"
   | "__CHECK_FSEQ2SAFE"
   | "__CHECK_FSEQALIGN"
-  | "__CHECK_SEQALIGN"
-  | "__CHECK_SEQ2SAFE"
-  | "__CHECK_BOUNDS_LEN"
-  | "__CHECK_INDEXALIGN"
-  | "__CHECK_GEU"
-  | "__CHECK_SEQ2FSEQ"
-  | "__CHECK_POSITIVE"
   | "__CHECK_FSEQARITH"
   | "__CHECK_FSEQARITH2SAFE"
+  | "__CHECK_FUNCTIONPOINTER"
+  | "__CHECK_GEU"
+  | "__CHECK_INDEXALIGN"
   | "__CHECK_NULL"
+  | "__CHECK_POSITIVE"
   | "__CHECK_RTTICAST"
+  | "__CHECK_SEQ2FSEQ"
+  | "__CHECK_SEQ2SAFE"
+  | "__CHECK_SEQALIGN"
+  | "__CHECK_STOREPTR"
+  | "__CHECK_STRINGMAX"
   | "__CHECK_WILDPOINTERREAD"
-  | "__CHECK_WILDPOINTERWRITE_NOSTACKCHECK"
   | "__CHECK_WILDPOINTERWRITE"
+  | "__CHECK_WILDPOINTERWRITE_NOSTACKCHECK"
   | "__CHECK_WILDP_WRITE_ATLEAST" ->
-      true
+      Check
+  | "fp_fail"
+  | "lbound_or_ubound_fail"
+  | "ubound_or_non_pointer_fail" ->
+      Fail
   | _ ->
-      false
+      Generic
 
 
 class visitor =
@@ -39,33 +46,72 @@ class visitor =
     method globals : global list = []
 
     method vfunc func =
-      if isCheck func.svar.vname then
-	SkipChildren
-      else
-	DoChildren
+      match classify func.svar.vname with
+      | Check
+      | Fail ->
+	  SkipChildren
+      | Generic ->
+	  DoChildren
 
-    method vstmt stmt =
-      begin
-	match stmt.skind with
-	  (* !!!: CHECK_RETURNPTR not yet handled *)
-	| Instr [Call (None, Lval (Var {vname = vname}, NoOffset), _, location)] ->
-	    if isCheck vname then
-	      sites <- stmt :: sites
-	    else if string_match checkPattern vname 0 then
-	      begin
-		currentLoc := get_stmtLoc stmt.skind;
-		ignore (warn "suspicious non-check call: %s\n" vname)
-	      end
+    method vstmt ({skind = skind} as stmt) =
+      match skind with
+      | Instr [Call (None, Lval (Var {vname = vname}, NoOffset), _, location)] ->
+	  begin
+	    match classify vname with
+	    | Check ->
+		sites <- stmt :: sites;
+	    | Fail ->
+		currentLoc := get_stmtLoc skind;
+		ignore (bug "found raw failure call; missed containing check?");
+	    | Generic ->
+		if string_match checkPattern vname 0 then
+		  begin
+		    currentLoc := get_stmtLoc skind;
+		    ignore (warn "suspicious non-check call: %s\n" vname)
+		  end
+	  end;
+	  SkipChildren
 
-	| Instr [_]
-	| Instr [] ->
-	    ()
+      | Instr [_]
+      | Instr [] ->
+	  SkipChildren
 
-	| Instr _ ->
-	    currentLoc := get_stmtLoc stmt.skind;
-	    ignore (bug "non-singleton Instr")
+      | Instr _ ->
+	  currentLoc := get_stmtLoc skind;
+	  ignore (bug "non-singleton Instr");
+	  SkipChildren
 
-	| _ -> ()
-      end;
-      DoChildren
+      | If (Lval (Var p, NoOffset),
+	    { battrs = [];
+	      bstmts = [
+	      { skind = If (BinOp (Shiftrt,
+				   BinOp (MinusPP,
+					  Lval (Var _, NoOffset),
+					  Lval (Var p', NoOffset),
+					  TInt (IInt, [])),
+				   Const (CInt64 (twentyOne, IInt, _)),
+				   TInt (IInt, [])),
+			    { battrs = []; bstmts = [] },
+			    { battrs = []; bstmts = [
+			      { skind = Block { battrs = [];
+						bstmts = [
+						{ skind = Instr [
+						  Call (_, Lval (Var {vname = "fp_fail"},
+								 NoOffset),
+							Const (CInt64 (zero, IInt, _)) :: _,
+							_) ] };
+						{ skind = Instr [] } ] } } ] },
+			    _) } ] },
+	    { battrs = []; bstmts = [] },
+	    _)
+	when twentyOne = Int64.of_int 21
+	    && zero = Int64.zero
+	    && p == p'
+	->
+	  (* CHECK_RETURNPTR *)
+	  sites <- stmt :: sites;
+	  SkipChildren
+
+      | _ ->
+	  DoChildren
   end

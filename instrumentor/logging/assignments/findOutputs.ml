82eab44a014149cbb39baa61cdd204ef4ec468df
Ben Liblit <liblit@cs.wisc.edu>
2002-09-23 23:13:04 +0000

    Match and ignore void-returning functions.
    Match and ignore zero-instruction Instr blocks.

--- instrumentor/logging/assignments/findOutputs.ml
+++ instrumentor/logging/assignments/findOutputs.ml
@@ -8,8 +8,12 @@ let collect instr =
     | Instr [Set (lval, _, location)]
     | Instr [Call (Some lval, _, _, location)] ->
 	Dissect.dissect lval (typeOfLval lval) outputs
+    | Instr [Call (None, _, _, _)]
+    | Instr [] ->
+	()
     | Instr _ ->
-	failwith "instr should have been atomized"
+	currentLoc := get_stmtLoc instr;
+	ignore (bug "instr should have been atomized")
     | _ ->
 	()
   end;

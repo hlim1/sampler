f67842bdda79404f84cb04c60a3c046058d30c9f
Ben Liblit <liblit@cs.wisc.edu>
2002-09-24 07:15:31 +0000

    Specialized CCured driver.  This uses CCured's Perl-based driver infrastructure.

---
+++ instrumentor/decure/driver/Decure.pm
@@ -0,0 +1,52 @@
+package Decure;
+
+use strict;
+use FindBin;
+use File::Basename;
+
+use lib '../../../../cil/bin';
+use lib '../../../../cil/lib';
+use CCured;
+our @ISA = qw(CCured);
+
+
+my $libcountdown = "$FindBin::Bin/../../../libcountdown";
+
+
+########################################################################
+
+
+sub preprocess_after_cil {
+    my $self = shift;
+    my $ppargs = pop;
+
+    my @ppargs = ('-include', "$FindBin::Bin/../libdecure/decure.h",
+		  '-include', "$libcountdown/countdown.h",
+		  @{$ppargs});
+    
+    $self->SUPER::preprocess_after_cil(@_, \@ppargs);
+}
+
+
+sub compile_cil {
+    my $self = shift;
+    my $input = shift;
+
+    my $base = basename $input, ".i";
+    my $output = "${base}_inst.c";
+    my $instrumentor = "$FindBin::Bin/../main";
+    $self->runShell("$instrumentor $input >$output");
+
+    $self->SUPER::compile_cil($output, @_);
+}
+
+
+sub link_after_cil {
+    my $self = shift;
+    my $ldargs = pop;
+
+    push @{$ldargs}, "-Wl,-rpath,$libcountdown";
+    push @{$ldargs}, "-L$libcountdown", '-lcountdown';
+
+    $self->SUPER::link_after_cil(@_, $ldargs);
+}

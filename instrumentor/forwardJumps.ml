7d3c1df8ff44d1978f79c142c2bc64eb51cfd7e2
Ben Liblit <liblit@cs.wisc.edu>
2002-09-26 09:46:29 +0000

    Factor out some common code.

--- instrumentor/forwardJumps.ml
+++ instrumentor/forwardJumps.ml
@@ -1,13 +1,13 @@
 open Cil
-open ClonesMap
 
 
 let patch clones =
+  let findClone = ClonesMap.findCloneOf clones in
   let patchOne jump =
     match jump.skind with
     | Goto (destination, location) ->
-	let clonedJump = findCloneOf clones jump in
-	let clonedDest = findCloneOf clones !destination in
+	let clonedJump = findClone jump in
+	let clonedDest = findClone !destination in
 	clonedJump.skind <- Goto (ref clonedDest, location)
     | _ ->
 	failwith "unexpected statement kind in forward jumps list"

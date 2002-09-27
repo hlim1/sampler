086db9cc5f39cbbc361f841ca995d1d937c6797d
Ben Liblit <liblit@cs.wisc.edu>
2002-09-27 00:41:23 +0000

    Retain all structural ancestors of reachable code.  Otherwise, we might blow away, say, an entire "if" statement that has a "goto" target in its "else" branch.

--- instrumentor/deadCode.ml
+++ instrumentor/deadCode.ml
@@ -1,6 +1,40 @@
 open Cil
 
 
+let rec markBlock { bstmts = bstmts } =
+  let rec markStmt stmt =
+    let markedSelf = stmt.sid == -2 in
+
+    let markIfChild childMarked =
+      if childMarked then
+	stmt.sid <- -2;
+      childMarked || markedSelf
+    in
+
+    match stmt.skind with
+    | Instr _
+    | Return _
+    | Goto _
+    | Break _
+    | Continue _ ->
+	markedSelf
+
+    | If (_, trueBranch, falseBranch, _) ->
+	let markedTrue = markBlock trueBranch in
+	let markedFalse = markBlock falseBranch in
+	markIfChild (markedTrue || markedFalse)
+
+    | Switch (_, body, _, _)
+    | Loop (body, _, _, _)
+    | Block body ->
+	markIfChild (markBlock body)
+  in
+  let folder marked stmt =
+    markStmt stmt || marked
+  in
+  List.fold_left folder false bstmts
+
+
 let visit func =
   match func.sbody.bstmts with
   | [] -> ()
@@ -16,8 +50,9 @@ let visit func =
       in
       mark entry;
 
+      ignore (markBlock func.sbody);
+
       let removeUnmarked = function
-	| { skind = Block _ }
 	| { sid = -2 } ->
 	    ()
 	| stmt ->

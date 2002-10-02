66e73328d1706bd239aeb7edb646b1bb015b8746
Ben Liblit <liblit@cs.wisc.edu>
2002-10-02 23:59:36 +0000

    Use inline functions more aggressively.
    Avoid using aliases of inline functions; I'm not sure if GCC treats
    those the way that I'd want it to.

--- instrumentor/decure/libdecure/decure.h
+++ instrumentor/decure/libdecure/decure.h
@@ -1,8 +1,13 @@
 #ifndef INCLUDE_libdecure_decure_h
 #define INCLUDE_libdecure_decure_h
 
+#include "../../../libcountdown/countdown.h"
 
-unsigned resetCountdown() __attribute__ ((alias ("getNextCountdown")));
+
+inline unsigned resetCountdown()
+{
+  return getNextCountdown();
+}
 
 
 #endif /* !INCLUDE_libdecure_decure_h */

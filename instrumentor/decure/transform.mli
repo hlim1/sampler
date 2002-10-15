49e1d72f8b43ab06cac5fc7df0653500b46342a9
Ben Liblit <liblit@cs.wisc.edu>
2002-10-15 23:55:40 +0000

    Whitespace.

--- instrumentor/decure/transform.mli
+++ instrumentor/decure/transform.mli
@@ -1,10 +1,11 @@
 open Cil
 
 
-class visitor : file -> object
-  inherit TransformVisitor.visitor
-  method private collector : fundec -> Collector.visitor
-end
+class visitor : file ->
+  object
+    inherit TransformVisitor.visitor
+    method private collector : fundec -> Collector.visitor
+  end
 
 
 val phase : TestHarness.phase

f22e15723d151feb75151fe8906a3f5bb55a09f5
Ben Liblit <liblit@cs.wisc.edu>
2002-10-31 03:11:10 +0000

    Add handling for "--only <foo>" flag.

---
+++ instrumentor/daikon/driver/Daikon.pm
@@ -0,0 +1,79 @@
+package Daikon;
+
+use strict;
+use FindBin;
+use File::Basename;
+
+use CilConfig;
+use Cilly;
+our @ISA = qw(Cilly);
+
+
+########################################################################
+
+
+my $daikon = "$FindBin::Bin/..";
+my $instrumentor = "$daikon/main";
+my $libdaikon = "$daikon/libdaikon";
+
+my $root = "$daikon/../..";
+my $libcountdown = "$root/libcountdown";
+
+
+########################################################################
+
+
+sub setDefaultArguments {
+    my $self = shift;
+    $self->SUPER::setDefaultArguments;
+    $self->{only} = '';
+    $self->{LD} = "libtool $self->{LD}";
+    $self->{TRACE_COMMANDS} = 0;
+}
+
+
+sub collectOneArgument {
+    my $self = shift;
+    my ($arg, $pargs) = @_;
+
+    if ($arg eq '--only') {
+	$self->{only} = shift @{$pargs};
+	return 1;
+    } else {
+	$self->SUPER::collectOneArgument(@_);
+    }
+}
+
+
+sub preprocess_before_cil {
+    my $self = shift;
+    my @ppargs = @{(pop)};
+
+    push @ppargs, ('-include', "$libcountdown/countdown.h",
+		   '-include', "$libcountdown/cyclic.h",
+		   '-include', "$libdaikon/daikon.h");
+    
+    $self->SUPER::preprocess_before_cil(@_, \@ppargs);
+}
+
+
+sub applyCil {
+    my ($self, $ppsrc, $dest) = @_;
+    
+    my ($base, $dir, undef) = fileparse $dest, '\\.[^.]+';
+    my $aftercil = "$dir$base.inst.c";
+
+    $self->runShell("$instrumentor --only '$self->{only}' @{$ppsrc} >$aftercil");
+    return $aftercil;
+}
+
+
+sub link_after_cil {
+    my $self = shift;
+    my @ldargs = @{(pop)};
+
+    push @ldargs, ("-L$libdaikon", '-ldaikon',
+		   "-L$libcountdown", '-lcyclic');
+
+    $self->SUPER::link_after_cil(@_, \@ldargs);
+}

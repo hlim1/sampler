a0c54323f34100d484a76ea9f7545de22cec4d1c
Ben Liblit <liblit@cs.wisc.edu>
2002-11-13 03:22:02 +0000

    Declarations for cyclic countdown support library.

---
+++ libcountdown/cyclic.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_libcountdown_cyclic_h
+#define INCLUDE_libcountdown_cyclic_h
+
+
+const unsigned *loadCountdowns(const char []);
+
+void unloadCountdowns(const unsigned *);
+
+
+#endif /* !INCLUDE_libcountdown_cyclic_h */

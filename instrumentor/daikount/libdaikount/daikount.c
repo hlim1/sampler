ec381dfc0325bdbd95afb345ec12b4f6385115f6
Ben Liblit <liblit@cs.wisc.edu>
2003-03-14 03:48:29 +0000

    Don't form the name of the log file until shutdown time.  Forked processes only run the initialization code once, so if we form the file name at initialization time, all of the children will use the same name.

--- instrumentor/daikount/libdaikount/daikount.c
+++ instrumentor/daikount/libdaikount/daikount.c
@@ -9,28 +9,68 @@
 
 
 const struct Invariant *invariants;
-static FILE *logFile;
 
 
 #define obstack_chunk_alloc malloc
 #define obstack_chunk_free free
 
 
+static const char *logFileName()
+{
+  return getenv("SAMPLER_FILE");
+}
+
+
+static FILE *openLogFile()
+{
+  struct obstack obstack;
+  obstack_init(&obstack);
+
+  const char *start = logFileName();
+  if (!start)
+    return 0;
+  
+  while (*start)
+    {
+      const char *end = strstr(start, "$$");
+      if (end)
+	{
+	  obstack_grow(&obstack, start, end - start);
+	  obstack_printf(&obstack, "%d", getpid());
+	  start = end + 2;
+	}
+      else
+	break;
+    }
+
+  const int leftovers = strlen(start);
+  obstack_grow0(&obstack, start, leftovers);
+  
+  char * const filename = (char *) obstack_finish(&obstack);
+  FILE * const logFile = fopen(filename, "w");
+  obstack_free(&obstack, filename);
+  return logFile;
+}
+
+
 static void dumpInvariants(int signum)
 {
-  const struct Invariant *invariant;
+  FILE * const logFile = openLogFile();
+  if (logFile)
+    {
+      fprintf(logFile, "%d\n", signum);
 
-  fprintf(logFile, "%d\n", signum);
+      const struct Invariant *invariant;
+      for (invariant = invariants; invariant; invariant = invariant->next)
+	fprintf(logFile, "%s\t%u\t%s\t%s\t%s\t%u\t%u\t%u\t%u\n",
+		invariant->file, invariant->line, invariant->function,
+		invariant->left, invariant->right, invariant->id,
+		invariant->counters[0], invariant->counters[1],
+		invariant->counters[2]);
 
-  for (invariant = invariants; invariant; invariant = invariant->next)
-    fprintf(logFile, "%s\t%u\t%s\t%s\t%s\t%u\t%u\t%u\t%u\n",
-	    invariant->file, invariant->line, invariant->function,
-	    invariant->left, invariant->right, invariant->id,
-	    invariant->counters[0], invariant->counters[1],
-	    invariant->counters[2]);
-
-  if (fclose(logFile))
-    perror("logger: fclose error");
+      if (fclose(logFile))
+	perror("logger: fclose error");
+    }
 }
 
 
@@ -50,55 +90,23 @@ static void handleSignal(int signum)
 
 __attribute__((constructor)) static void initialize()
 {
-  const char * const envar = getenv("SAMPLER_FILE");
-  if (!envar)
+  if (logFileName())
+    {
+      signal(SIGABRT, handleSignal);
+      signal(SIGBUS, handleSignal);
+      signal(SIGFPE, handleSignal);
+      signal(SIGSEGV, handleSignal);
+      signal(SIGTRAP, handleSignal);
+    }
+  else
     {
       fputs("logger: no $SAMPLER_FILE given in environment\n", stderr);
       return;
     }
-
-  {
-    struct obstack obstack;
-    obstack_init(&obstack);
-
-    const char *start = envar;
-    while (*start)
-      {
-	const char *end = strstr(start, "$$");
-	if (end)
-	  {
-	    obstack_grow(&obstack, start, end - start);
-	    obstack_printf(&obstack, "%d", getpid());
-	    start = end + 2;
-	  }
-	else
-	  break;
-      }
-
-    const int leftovers = strlen(start);
-    obstack_grow0(&obstack, start, leftovers);
-  
-    char * const filename = (char *) obstack_finish(&obstack);
-    logFile = fopen(filename, "w");
-    obstack_free(&obstack, filename);
-  }
-
-  if (!logFile)
-    {
-      perror("logger: fopen error");
-      return;
-    }
-      
-  signal(SIGABRT, handleSignal);
-  signal(SIGBUS, handleSignal);
-  signal(SIGFPE, handleSignal);
-  signal(SIGSEGV, handleSignal);
-  signal(SIGTRAP, handleSignal);
 }
 
 
 __attribute__((destructor)) static void shutdown()
 {
-  if (logFile)
-    dumpInvariants(0);
+  dumpInvariants(0);
 }

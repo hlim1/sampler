62dea7f4affae0a9003763ec9c2bd1150a747d2e
Alice Zheng <alicez@gmail.com>
2003-04-04 21:36:23 +0000

    Added files: ds1000ngood_plot.mat  -- matlab data file for figure ds1000ngood_plot.eps  -- eps figure ngood_plot.m          -- matlab script to generate figure

---
+++ paper/applications/ngood_plot.m
@@ -0,0 +1,8 @@
+load ds1000ngood_plot.mat
+errorbar(S, mean_ngood, std_ngood);
+hold on; plot(S, mean_ngood, 'rx');
+xlabel('Number of successful trials used', 'fontsize', 12);
+ylabel('Number of "good" features left', 'fontsize', 12);
+grid on;
+print -f1 -deps ds1000ngood_plot.eps
+

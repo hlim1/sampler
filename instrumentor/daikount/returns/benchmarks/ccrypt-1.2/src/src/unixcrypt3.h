de34aae19576cbefa05697cc754aeff023bec4c2
Ben Liblit <liblit@cs.wisc.edu>
2003-04-04 05:31:10 +0000

    *** empty log message ***

--- instrumentor/daikount/returns/benchmarks/ccrypt-1.2/src/src/unixcrypt3.h
+++
@@ -1,8 +0,0 @@
-/* $Id: unixcrypt3.h,v 1.1 2003/03/27 02:48:44 liblit Exp $ */
-
-#ifndef __UNIXCRYPT3_H
-#define __UNIXCRYPT3_H
-
-char *crypt_replacement (const char *key, const char *salt);
-
-#endif /* __UNiXCRYPT3_H */

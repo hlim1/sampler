d323020fc82133dd44a5d68d2a5ed3eb5c1a6141
Alice Zheng <alicez@gmail.com>
2003-04-07 07:00:37 +0000

    Revised ngood eps, mat, and m files.  Previous versions must have been using the wrong dataset.

--- paper/applications/ngood_plot.m
+++ paper/applications/ngood_plot.m
@@ -3,7 +3,7 @@ errorbar(S, mean_ngood, std_ngood);
 hold on; plot(S, mean_ngood, 'rx');
 xlabel('Number of successful trials used', 'fontsize', 12);
 ylabel('Number of "good" features left', 'fontsize', 12);
+set(gca, 'ytick', 0:20:150);
+axis([-50 3000 0 150]);
 grid on;
-axis([-50 4000 -20 180]);
-print -f1 -deps ds1000ngood_plot.eps
-
+print -f1 -deps ccrypt-data/ds1000ngood_plot.eps

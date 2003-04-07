b6a4475d7696b647dd30802c6dc182af812bda99
Alice Zheng <alicez@gmail.com>
2003-04-07 04:19:53 +0000

    New ngood_plot.m

--- paper/applications/ngood_plot.m
+++ paper/applications/ngood_plot.m
@@ -4,5 +4,6 @@ hold on; plot(S, mean_ngood, 'rx');
 xlabel('Number of successful trials used', 'fontsize', 12);
 ylabel('Number of "good" features left', 'fontsize', 12);
 grid on;
+axis([-50 4000 -20 180]);
 print -f1 -deps ds1000ngood_plot.eps
 

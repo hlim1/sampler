60eadea5bab152de8922c9dd0f1ba99526aa6f40
Ben Liblit <liblit@cs.wisc.edu>
2003-04-07 09:08:38 +0000

    Use slightly larger axis labels, as found in the performance bar plots.

--- paper/applications/ngood_plot.m
+++ paper/applications/ngood_plot.m
@@ -1,8 +1,8 @@
 load ds1000ngood_plot.mat
 errorbar(S, mean_ngood, std_ngood);
 hold on; plot(S, mean_ngood, 'rx');
-xlabel('Number of successful trials used', 'fontsize', 12);
-ylabel('Number of "good" features left', 'fontsize', 12);
+xlabel('Number of successful trials used', 'fontsize', 14);
+ylabel('Number of "good" features left', 'fontsize', 14);
 set(gca, 'ytick', 0:20:150);
 axis([-50 3000 0 150]);
 grid on;

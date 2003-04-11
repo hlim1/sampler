3cc3ac40cb224e694dfb9d6918752c0a15279f12
Ben Liblit <liblit@cs.wisc.edu>
2003-04-11 00:21:17 +0000

    Remove ccrypt performance bar graph.  Treat this relatively minor point in prose alone.

--- paper/applications/ccrypt.mk
+++ paper/applications/ccrypt.mk
@@ -4,21 +4,3 @@ ds1000ngood_plot.eps: %.eps: ngood_plot.m %.mat
 	rm -f $@
 	$(matlab) -r $(basename $<)
 	[ -r $@ ]
-
-
-########################################################################
-
-
-dir := $(benchdir)/ccrypt-1.2
-
-
-eps += ccrypt_density
-
-ccrypt_density.txt: density.pl $(dir)/collated-times.txt
-	./$^ >$@.
-	mv $@. $@
-
-ccrypt_density.eps: %.eps: %.m %.txt
-	rm -f $@
-	$(matlab) -r $*
-	[ -r $@ ]
--- paper/applications/ccrypt_density.m
+++
@@ -1,3 +0,0 @@
-data = load('ccrypt_density.txt');
-plot_density;
-print -f1 -deps 'ccrypt_density.eps';

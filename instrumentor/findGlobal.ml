bcf52788ffc6e357225d6293a0fae3fca1176d9a
Ben Liblit <liblit@cs.wisc.edu>
2003-06-21 00:05:05 +0000

    When looking for a global, finding a definition is just as good as finding a declaration.  Return either.

--- instrumentor/findGlobal.ml
+++ instrumentor/findGlobal.ml
@@ -9,6 +9,9 @@ let find name {globals = globals} =
     | GVarDecl ({vname = vname} as varinfo, _) :: _
       when vname = name ->
 	varinfo
+    | GVar ({vname = vname} as varinfo, _, _) :: _
+      when vname = name ->
+	varinfo
     | _ :: rest ->
 	findAmong rest
     | [] ->

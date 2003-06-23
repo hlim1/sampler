534e39624e1ef98f0d12bab78a8afd074355558e
Ben Liblit <liblit@cs.wisc.edu>
2003-06-23 23:13:47 +0000

    Indentation fixup.

--- instrumentor/branches/branchClassifier.ml
+++ instrumentor/branches/branchClassifier.ml
@@ -2,35 +2,35 @@ open Cil
 
 
 class visitor (counters : Counters.builder) func =
-    object (self)
-      inherit Classifier.visitor as super
+  object (self)
+    inherit Classifier.visitor as super
 
-      val mutable sites = []
-      method sites = sites
+    val mutable sites = []
+    method sites = sites
 
-      method private patchConditional stmt =
-	begin
-	  match stmt.skind with
-	  | If (predicate, thenClause, elseClause, location) ->
-	      let (lval, site) = counters#bump func location predicate in
-	      let site = mkStmt site in
-	      sites <- site :: sites;
-	      stmt.skind <- Block (mkBlock [mkStmtOneInstr (Set (lval, predicate, location));
-					    site;
-					    mkStmt (If (Lval lval,
-							thenClause,
-							elseClause,
-							location))])
-	  | _ ->
-	      failwith "cannot patch non-conditional"
-	end;
-	stmt
-
-      method vstmt stmt =
-	let action = super#vstmt stmt in
+    method private patchConditional stmt =
+      begin
 	match stmt.skind with
-	| If _ ->
-	    ChangeDoChildrenPost (stmt, self#patchConditional)
+	| If (predicate, thenClause, elseClause, location) ->
+	    let (lval, site) = counters#bump func location predicate in
+	    let site = mkStmt site in
+	    sites <- site :: sites;
+	    stmt.skind <- Block (mkBlock [mkStmtOneInstr (Set (lval, predicate, location));
+					  site;
+					  mkStmt (If (Lval lval,
+						      thenClause,
+						      elseClause,
+						      location))])
 	| _ ->
-	    action
-    end
+	    failwith "cannot patch non-conditional"
+      end;
+      stmt
+
+    method vstmt stmt =
+      let action = super#vstmt stmt in
+      match stmt.skind with
+      | If _ ->
+	  ChangeDoChildrenPost (stmt, self#patchConditional)
+      | _ ->
+	  action
+  end

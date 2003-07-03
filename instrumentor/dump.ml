0c791559351bfcc1be31abea87e1e70c6644fbb1
Ben Liblit <liblit@cs.wisc.edu>
2003-07-03 03:13:56 +0000

    Remove some leftover debugging code.

--- instrumentor/dump.ml
+++ instrumentor/dump.ml
@@ -3,9 +3,6 @@ open Cil
 let check = Check.checkFile []
 
 let process filename =
-  let s = mkString "foo" in
-  ignore (Pretty.eprintf "literal string has type %a@!" d_type (typeOf s));
-  ignore (Pretty.eprintf "fixed export is type %a@!" d_type charConstPtrType);
   let file = Frontc.parse filename () in
   Rmtmps.removeUnusedTemps file;
   dumpFile defaultCilPrinter stdout file

d9b36af43fd1b0dee58deab90eb1f525e9e9d7f8
Ben Liblit <liblit@cs.wisc.edu>
2003-07-25 07:51:22 +0000

    After cloning a switch statement, fix up the clone's case list to point to the cloned cases rather than the original cases.  This might not matter, but it feels safer.

--- instrumentor/duplicate.ml
+++ instrumentor/duplicate.ml
@@ -11,24 +11,38 @@ let cloneLabel = function
 
 
 class visitor pairs =
-  object
+  object (self)
     inherit FunctionBodyVisitor.visitor
 
-    method vstmt stmt =
-      let clone = { stmt with labels = mapNoCopy cloneLabel stmt.labels }
-      in
-      
-      if stmt.sid != -1 then
-	pairs.(stmt.sid) <- (stmt, clone);
+    method private fixup statement =
+      begin
+	assert (statement == snd pairs.(statement.sid));
+	match statement.skind with
+	| Switch (expression, body, cases, location) ->
+	    let clonedCases =
+	      let mapper case =
+		assert (case == fst pairs.(case.sid));
+		snd pairs.(case.sid)
+	      in
+	      List.map mapper cases
+	    in
+	    statement.skind <- Switch (expression, body, clonedCases, location)
+	| _ ->
+	    ()
+      end;
+      statement
 
-      ChangeDoChildrenPost (clone, (fun s -> s))
+    method vstmt stmt =
+      let clone = { stmt with labels = mapNoCopy cloneLabel stmt.labels } in
+      pairs.(stmt.sid) <- (stmt, clone);
+      ChangeDoChildrenPost (clone, self#fixup)
   end
 
 
 let duplicateBody fundec =
   match fundec.smaxstmtid with
   | None ->
-      failwith "no maximum statement id; please call Cfg.prepare before duplicating"
+      failwith "no maximum statement id; please call Cfg.build before duplicating"
   | Some max ->
       let pairs = Array.make max (dummyStmt, dummyStmt) in
       let visitor = new visitor pairs in

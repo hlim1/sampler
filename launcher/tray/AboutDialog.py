913316d1f0baf344560fee2dbc6af3bf76ec8d86
Ben Liblit <liblit@cs.wisc.edu>
2003-09-05 07:19:27 +0000

    Manually set some About box properties that libglade doesn't install for us.

--- launcher/tray/AboutDialog.py
+++ launcher/tray/AboutDialog.py
@@ -2,9 +2,16 @@ import gtk.gdk
 import gtk.glade
 
 from LazyDialog import LazyDialog
+
+import Config
 import Paths
 
 
 class AboutDialog(LazyDialog):
     def __init__(self, client):
         LazyDialog.__init__(self, client, 'about')
+
+    def populate(self, xml, widget):
+        LazyDialog.populate(self, xml, widget)
+        widget.set_property('name', 'Feedback Collector')
+        widget.set_property('version', Config.version)

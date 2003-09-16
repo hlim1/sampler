7a11a826c46bc4079157a1230417d12017b8594c
Ben Liblit <liblit@cs.wisc.edu>
2003-09-16 03:49:45 +0000

    *** empty log message ***

--- launcher/tray/Main.py
+++ launcher/tray/Main.py
@@ -15,6 +15,7 @@ from UploaderTrayIcon import UploaderTrayIcon
 
 import Config
 import Keys
+import Paths
 import monitor
 
 
@@ -25,13 +26,11 @@ gnome.program_init('tray', Config.version)
 client = gconf.client_get_default()
 dir = GConfDir(client, Keys.root, gconf.CLIENT_PRELOAD_ONELEVEL)
 
-factory = Factory()
-
-tray = UploaderTrayIcon(client)
-tray.show_all()
-
 if not client.get_bool(Keys.asked):
-    first_time = os.path.join('@first_timedir@', 'first-time')
+    first_time = os.path.join(Paths.first_timedir, 'first-time')
     os.spawnl(os.P_NOWAIT, first_time, first_time)
 
+factory = Factory()
+tray = UploaderTrayIcon(client)
+tray.show_all()
 bonobo.main()

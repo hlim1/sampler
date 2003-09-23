4b3496e710880fa980decd7689e4bb6a4028481e
Ben Liblit <liblit@cs.wisc.edu>
2003-09-23 00:01:37 +0000

    Add in entries for runs with no samples.  There might be a more clever way to do this.

--- mining/queries/longest-runs.sql
+++ mining/queries/longest-runs.sql
@@ -1,6 +1,15 @@
 -- runs ordered by sum of predicate counts, largest first
 
+CREATE TEMPORARY TABLE results AS
 SELECT run_id, build.*, sum(count) as weight
 FROM run_sample NATURAL JOIN build
-GROUP BY run_id
+GROUP BY run_id;
+
+ALTER TABLE results ADD PRIMARY KEY (run_id);
+
+INSERT IGNORE INTO results
+SELECT run_id, build.*, 0
+FROM run NATURAL JOIN build;
+
+SELECT * FROM results
 ORDER BY weight DESC;

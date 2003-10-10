7a5f3e6c2d51b79f3d5a48f011fa0966ccdf3d4b
Ben Liblit <liblit@cs.wisc.edu>
2003-10-10 23:50:08 +0000

    Blow away complicated data structures in favor of a simple string.

--- lib/embedded-cfg.h
+++ lib/embedded-cfg.h
@@ -2,33 +2,7 @@
 #define INCLUDE_sampler_embeded_cfg_h
 
 
-struct __CFG_func {
-  const char * const name;
-  const unsigned nodeCount;
-  const struct __CFG_node * const nodes;
-  const unsigned flowCount;
-  const struct __CFG_flow * const flows;
-  const unsigned callCount;
-  const struct __CFG_call * const calls;
-};
-
-
-struct __CFG_node {
-  const char *file;
-  unsigned line;
-};
-
-
-struct __CFG_flow {
-  unsigned start;
-  unsigned finish;
-};
-
-
-struct __CFG_call {
-  unsigned caller;
-  const struct __CFG_func *callee;
-};
+#pragma cilnoremove("samplerCFG")
 
 
 #endif /* !INCLUDE_sampler_embeded_cfg_h */

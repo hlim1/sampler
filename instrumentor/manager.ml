e4677abac3bfb8944de00b555fc57fb81ef0dc23
Ben Liblit <liblit@cs.wisc.edu>
2003-10-14 06:42:38 +0000

    When in phase timing mode, report how long it takes to perform various subphases of instrumentation.

--- instrumentor/manager.ml
+++ instrumentor/manager.ml
@@ -34,26 +34,30 @@ class virtual visitor schemeName file =
       FunctionFilter.filter#collectPragmas file;
 
       let allSites =
-	let folder others = function
-	  | GFun (func, _) ->
-	      begin
-		match self#classifyStatements func with
-		| [] -> others
+	TestHarness.time "  classifying / inserting instrumentation"
+	  (fun () ->
+	    let folder others = function
+	      | GFun (func, _) ->
+		  begin
+		    match self#classifyStatements func with
+		    | [] -> others
 		| sites -> (func, sites) :: others
-	      end
-	  | _ ->
-	      others
-	in
-	foldGlobals file folder []
+		  end
+	      | _ ->
+		  others
+	    in
+	    foldGlobals file folder [])
       in
 
       EmbedCFG.visit file;
-      self#finalize;
+
+      TestHarness.time "  finalizing" (fun () -> self#finalize);
 
       if !sample then
 	begin
 	  let tester = Weighty.collect file allSites in
 	  let countdown = new Countdown.countdown file in
-	  List.iter (Transform.visit tester countdown) allSites
+	  TestHarness.time "  applying sampling transformation"
+	  (fun () -> List.iter (Transform.visit tester countdown) allSites)
 	end
   end

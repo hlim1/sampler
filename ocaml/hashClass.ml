936f9559b7ff30c81dabdb4f5cc5b27904be20f8
Ben Liblit <liblit@cs.wisc.edu>
2003-10-22 06:39:47 +0000

    Class wrapper for non-functor-based hash tables.

---
+++ ocaml/hashClass.ml
@@ -0,0 +1,14 @@
+class ['key, 'value] t initial =
+  object
+    val storage : ('key, 'value) Hashtbl.t = Hashtbl.create initial
+
+    method add = Hashtbl.add storage
+
+    method find = Hashtbl.find storage
+
+    method findAll = Hashtbl.find_all storage
+
+    method mem = Hashtbl.mem storage
+
+    method iter visitor = Hashtbl.iter visitor storage
+  end
---
+++ ocaml/hashClass.mli
@@ -0,0 +1,10 @@
+class ['key, 'value] t : int ->
+  object
+    method add : 'key -> 'value -> unit
+
+    method find : 'key -> 'value
+    method findAll : 'key -> 'value list
+    method mem : 'key -> bool
+
+    method iter : ('key -> 'value -> unit) -> unit
+  end

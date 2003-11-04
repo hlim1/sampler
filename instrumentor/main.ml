132dec52b272c6f83ef802fd6ea24dc344cec2aa
Ben Liblit <liblit@cs.wisc.edu>
2003-11-04 08:12:09 +0000

    Slurp all instrumentation schemes' OCaml code up into the instrumentor subdirectory itself.  This makes library and link management much simpler.

--- instrumentor/main.ml
+++ instrumentor/main.ml
@@ -1,8 +1,27 @@
+open Cil
+
+
 let _ =
+
+  let phases =
+    [
+     "removing unused symbols (early)", (fun file -> Rmtmps.removeUnusedTemps file);
+     Instrumentor.phase;
+     "removing unused symbols (late)", (fun file -> Rmtmps.removeUnusedTemps file);
+     Idents.phase;
+     "printing transformed code", dumpFile (new Printer.printer) stdout
+   ]
+  in
+
+  initCIL ();
+  Ptranal.conservative_undefineds := false;
+  lineDirectiveStyle := Some LinePreprocessorOutput;
 
   NothingScheme.register ();
   ScalarPairScheme.register ();
   BranchScheme.register ();
   ReturnScheme.register ();
 
-  Phases.main ()
+  Arg.parse (Options.argspecs ())
+    (TestHarness.doOne phases)
+    ("Usage:" ^ Sys.executable_name ^ " [<flag> | <source>] ...")

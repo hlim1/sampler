f7a8e52c1ebfc72e3c8da153cab20a3548e37f2d
Ben Liblit <liblit@cs.wisc.edu>
2003-11-05 02:08:15 +0000

    Remove a header which has been replaced by new stuff in "lib" directory.

--- instrumentor/branches/branches.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDE_libbranches_branches_h
-#define INCLUDE_libbranches_branches_h
-
-
-#pragma cilnoremove("branchesCounterTuples")
-static unsigned branchesCounterTuples[][2];
-
-
-#endif /* !INCLUDE_libbranches_branches_h */

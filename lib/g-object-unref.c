e7bef68be164d4ea25dae9482a27603b055a8cb2
Ben Liblit <liblit@cs.wisc.edu>
2004-03-21 01:32:39 +0000

    Split out GObject classification function into its own object file. This object will only be pulled in when linking an executable that really does call g_object_unref().  This way we avoid introducing a libgobject-2.0 link dependency that may not have been there before.

--- lib/g-object-unref.c
+++ lib/g-object-unref.c
@@ -1,4 +1,3 @@
-#include <glib-object.h>
 #include "report.h"
 #include "g-object-unref-types.h"
 
@@ -9,21 +8,4 @@ void gObjectUnrefReport(const unsigned char *signature,
   samplesBegin(signature, "g-object-unref");
   samplesDump4(count, tuples);
   samplesEnd();
-}
-
-
-unsigned gObjectUnrefClassify(gpointer chaff)
-{
-  if (G_LIKELY(G_IS_OBJECT(chaff)))
-    switch (((GObject *) chaff)->ref_count)
-      {
-      case 0:
-	return 0;
-      case 1:
-	return 1;
-      default:
-	return 2;
-      }
-  else
-    return 3;
 }

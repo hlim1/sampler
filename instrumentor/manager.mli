6801eb13085a4cf6fe549a3ec1b3a8ffd249d971
(no author) <(no author)>
2004-03-29 06:48:11 +0000

    This commit was manufactured by cvs2svn to create tag 'gimp-release-2-0-1--1-sam-1'.

--- instrumentor/manager.mli
+++
@@ -1,8 +0,0 @@
-open Cil
-
-
-class virtual visitor : string -> file ->
-  object
-    method private virtual statementClassifier : fundec -> Classifier.visitor
-    method private finalize : Digest.t Lazy.t -> unit
-  end

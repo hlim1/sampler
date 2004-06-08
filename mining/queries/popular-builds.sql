6e4354cbc9f23d8d64e7c742ba01058788ebcd47
Ben Liblit <liblit@cs.wisc.edu>
2004-06-08 05:50:59 +0000

    Ignore suppressed builds.
    Be more choosy about which columns to display.

--- mining/queries/popular-builds.sql
+++ mining/queries/popular-builds.sql
@@ -1,6 +1,7 @@
 -- find builds with the most runs
 
-SELECT build.*, count(*) AS repeats
+SELECT application_name, application_version, application_release, build_date, count(*) as repeats
 FROM run NATURAL JOIN build
+WHERE suppress IS NULL
 GROUP BY run.build_id
 ORDER BY repeats DESC;

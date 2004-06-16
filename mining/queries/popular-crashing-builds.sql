670fb27fb03f28edc36c351cab16cdc5f3e7c5f7
Ben Liblit <liblit@cs.wisc.edu>
2004-06-16 22:05:02 +0000

    Find individual builds with the most failures.

---
+++ mining/queries/popular-crashing-builds.sql
@@ -0,0 +1,7 @@
+-- find builds with the most crashes
+
+SELECT application_name, application_version, application_release, build_date, count(*) as reports, sum(!!exit_signal) as crashes
+FROM run NATURAL JOIN build
+WHERE !(application_name = 'evolution' AND build_date < '2003-8-25' and exit_signal)
+GROUP BY build.build_id
+ORDER BY crashes DESC;

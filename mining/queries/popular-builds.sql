64e1df019e1c0f9611504e10d44b3f389ee56c2b
Ben Liblit <liblit@cs.wisc.edu>
2004-06-17 04:58:03 +0000

    Rename suppression field in build table to allow easier natural joins with run table.

--- mining/queries/popular-builds.sql
+++ mining/queries/popular-builds.sql
@@ -2,6 +2,6 @@
 
 SELECT application_name, application_version, application_release, build_date, count(*) as repeats
 FROM run NATURAL JOIN build
-WHERE suppress IS NULL
+WHERE build_suppress IS NULL
 GROUP BY run.build_id
 ORDER BY repeats DESC;

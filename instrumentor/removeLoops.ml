d00e8583165a80f23db3329d4f39db9c9dba1c70
Ben Liblit <liblit@cs.wisc.edu>
2004-07-27 05:33:14 +0000

    Flattened out pattern matching and made more case-specific error messages.

--- instrumentor/removeLoops.ml
+++ instrumentor/removeLoops.ml
@@ -11,20 +11,21 @@ class visitor =
 
     method vstmt stmt =
       begin
-	match stmt.skind with
-	| Loop (block, location, _, _) ->
+	match stmt with
+	| { skind = Loop (block, location, _, _) } ->
 	    let goto = mkStmt (buildGoto stmt location) in
 	    block.bstmts <- block.bstmts @ [goto];
 	    stmt.skind <- Block block
-	| Break location when stmt.succs = [] ->
+	| { skind = Break location; succs = [] } ->
 	    stmt.skind <- Return (None, location)
-	| Break location
-	| Continue location ->
-	    begin
-	      match stmt.succs with
-	      | [target] -> stmt.skind <- buildGoto target location
-	      | _ -> failwith "break or continue must have exactly one successor"
-	    end
+	| { skind = Break location; succs = [target] } ->
+	    stmt.skind <- buildGoto target location
+	| { skind = Break _ } ->
+	    failwith "break cannot have more than one successor"
+	| { skind = Continue location; succs = [target] } ->
+	    stmt.skind <- buildGoto target location
+	| { skind = Continue _ } ->
+	    failwith "continue must have exactly one successor"
 	| _ -> ()
       end;
       DoChildren

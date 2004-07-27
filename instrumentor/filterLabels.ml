beede6e70f0c54dedb6a6b7b9e9c33721816c6af
Ben Liblit <liblit@cs.wisc.edu>
2004-07-27 07:17:51 +0000

    Take advantage of our handy label list formatter.

--- instrumentor/filterLabels.ml
+++ instrumentor/filterLabels.ml
@@ -49,14 +49,7 @@ class collectDestinations =
 	match stmt.skind with
 	| Goto (dest, _) ->
 	    if !dest.sid == -1 then
-	      begin
-		prerr_string "out-of-tree destination for labels:";
-		let printLabel = function
-		  | Label (name, _, _) -> prerr_string (" " ^ name)
-		  | _ -> ()
-		in
-		List.iter printLabel !dest.labels
-	      end
+	      ignore (bug "out-of-tree destination for labels: %a" Utils.d_labels !dest.labels)
 	    else
 	      destinations#add !dest ()
 	| _ -> ()

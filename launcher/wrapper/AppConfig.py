b2c4e7846cd437512263a1507323fd15719fccdb
Ben Liblit <liblit@cs.wisc.edu>
2005-03-22 02:00:33 +0000

    Oops, a trailing comma turned a simple string into a unary tuple.

--- launcher/wrapper/AppConfig.py
+++ launcher/wrapper/AppConfig.py
@@ -2,7 +2,7 @@ class AppConfig:
     '''Static configuration information about a single instrumented application'''
 
     def __init__(self, name, executable, debug_reporter, upload_headers):
-        upload_headers['application-name'] = name,
+        upload_headers['application-name'] = name
         upload_headers['executable-path'] = executable
         self.executable = executable
         self.debug_reporter = debug_reporter

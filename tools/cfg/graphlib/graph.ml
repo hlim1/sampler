76387ea2af0b1ca4594d52da8fba8945507fca73
Ben Liblit <liblit@cs.wisc.edu>
2005-04-17 22:16:12 +0000

    Minor informational message tweak.

--- tools/cfg/graphlib/graph.ml
+++ tools/cfg/graphlib/graph.ml
@@ -5,7 +5,7 @@ type ('node, 'edge) edge = ('node * 'edge * 'node)
 
 
 class ['node, 'nodeData, 'edge] graph nodeCount edgeCount =
-  let _ = Printf.eprintf "build graph with %d, %d nodes, edges\n%!" nodeCount edgeCount in
+  let _ = Printf.eprintf "build graph with %d nodes and %d edges\n%!" nodeCount edgeCount in
   object (self)
     val mutable nodes = new HashClass.c nodeCount
     val mutable edges = new HashClass.c edgeCount

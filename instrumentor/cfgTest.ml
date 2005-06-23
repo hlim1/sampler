fad97d6201f40ecdc81c66bbd49ee5342a889fd9
Ben Liblit <liblit@cs.wisc.edu>
2005-06-23 03:52:44 +0000

    CFG embedder needs a file digest, from which it generates the compilation unit signature.

--- instrumentor/cfgTest.ml
+++ instrumentor/cfgTest.ml
@@ -2,7 +2,8 @@ open Cil
 
 let process filename =
   let file = Frontc.parse filename () in
-  EmbedCFG.visit file;
+  let digest = lazy (Digest.file file.fileName) in
+  EmbedCFG.visit file digest;
   dumpFile defaultCilPrinter stdout file
 
 ;;

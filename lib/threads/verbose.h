c146232fb6476ce83e2b50e60531be12682d7fe8
Ben Liblit <liblit@cs.wisc.edu>
2006-06-21 05:33:29 +0000

    Report process id number in each verbose message.

--- lib/threads/verbose.h
+++ lib/threads/verbose.h
@@ -2,17 +2,19 @@
 #define INCLUDE_sampler_lib_threads_verbose_h
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
 void samplerInitializeVerbose(void);
 
-#define VERBOSE(format, ...)					\
-  do								\
-    {								\
-      extern int samplerVerbose;				\
-      if (__builtin_expect(samplerVerbose != 0, 0))		\
-	fprintf(stderr, "CBI: " format, ## __VA_ARGS__);	\
-    }								\
+#define VERBOSE(format, ...)						\
+  do									\
+    {									\
+      extern int samplerVerbose;					\
+      if (__builtin_expect(samplerVerbose != 0, 0))			\
+	fprintf(stderr, "CBI: pid %d: " format, getpid(), ## __VA_ARGS__); \
+    }									\
   while (0)
 
 

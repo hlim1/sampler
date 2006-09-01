c0311432944150f7053ac7be352bd28e75972660
Ben Liblit <liblit@cs.wisc.edu>
2006-09-01 01:46:37 +0000

    Reorderings, renamings, and assorted code tweaks to clean things up a bit.

--- ocaml.py
+++ ocaml.py
@@ -11,61 +11,39 @@ from SCons.Scanner import Scanner
 from utils import read_pipe
 
 
-#__pychecker__ = 'no-argsused'
-
-
 ########################################################################
 
 
-source_to_object = { False: {'.mli': '.cmi', '.ml': '.cmo'},
-                     True:  {'.mli': '.cmi', '.ml': '.cmx'} }
-object_to_source = { '.cmi': '.mli', '.cmo': '.ml', '.cmx': '.ml' }
+__source_to_object = {
+    False: {'.mli': '.cmi', '.ml': '.cmo'},
+    True:  {'.mli': '.cmi', '.ml': '.cmx'}
+    }
+__object_to_source = { '.cmi': '.mli', '.cmo': '.ml', '.cmx': '.ml' }
 
 
-def strs(items):
-    return map(str, items)
-
-
-def warn(message):
+def __warn(message):
     print >>sys.stderr, 'scons-ocaml: warning:', message
 
 
-def ocaml_path_function(env, node):
-    __pychecker__ = 'no-argsused'
-    stdlib = env['OCAML_STDLIB']
-    if stdlib is None:
-        ocamlc = env['OCAMLC']
-        ocamlc = env.WhereIs(ocamlc)
-        prefix = dirname(dirname(ocamlc))
-        stdlib = '%s/lib/ocaml' % prefix
-
-    return tuple(env['OCAML_PATH'] + [stdlib])
-
-
 ########################################################################
+#
+#  ocamldep dependency scanner
+#
+#  This scanner runs ocamldep on ".ml" or ".mli" files.  It parses
+#  ocamldep's output to idenfiy other modules that the scanned file
+#  depends upon.
+#
 
 
-def obj_emitter(target, source, env):
-    suffix = { False: '.cmo', True: '.cmx' }[env['OCAML_NATIVE']]
-    cmos = [ node for node in target if node.get_suffix() == suffix ]
-    if env['OCAML_DTYPES']:
-        annots = ( node.target_from_source('', '.annot') for node in cmos )
-        target += annots
-    if env['OCAML_NATIVE']:
-        ofiles = ( node.target_from_source('', '.o') for node in cmos )
-        target += ofiles
-    return target, source
-
-
-def ocamldep(node, env, path):
+def __ocamldep_scan(node, env, path):
     __pychecker__ = 'no-argsused'
     suffix = node.get_suffix()
-    if not suffix in source_to_object[env['OCAML_NATIVE']]:
-        warn('%s does not have a suitable suffix' % node)
+    if not suffix in __source_to_object[env['OCAML_NATIVE']]:
+        __warn('%s does not have a suitable suffix' % node)
         return
 
     if not node.exists():
-        warn('%s does not exist' % node)
+        __warn('%s does not exist' % node)
 
     stdout = read_pipe([['$OCAMLDEP', '$_OCAML_PATH', '$_OCAML_PP', str(node)]], env)
     if stdout == 0:
@@ -73,13 +51,14 @@ def ocamldep(node, env, path):
     elif isinstance(stdout, int):
         env.Exit(stdout)
 
-    target = node.target_from_source('', source_to_object[env['OCAML_NATIVE']][suffix])
+    target = node.target_from_source('', __source_to_object[env['OCAML_NATIVE']][suffix])
     target = str(target) + ':'
 
     def joinLines(stream):
+        """reassemble long lines that were split using backslashed newlines"""
         accum = ''
         for line in stream:
-            if line[-2] == '\\':
+            if line[-2:] == '\\\n':
                 accum += line[0:-2]
             else:
                 yield accum + line
@@ -93,87 +72,112 @@ def ocamldep(node, env, path):
     return deps
 
 
-ocamldep_scanner = Scanner(function=ocamldep, name='ocamldep',
-                           path_function=ocaml_path_function,
-                           skeys=['.mli', '.ml'],
-                           node_class=File)
-
-
-def obj_suffix(env, sources):
-    [source] = sources
-    result = source_to_object[env['OCAML_NATIVE']][source.get_suffix()]
-    return result
-
-
-obj_action = Action([['$_OCAMLC', '$_OCAML_DEBUG', '$_OCAML_DTYPES', '$_OCAML_PATH', '$_OCAML_PP', '$_OCAML_WARN', '$_OCAML_WARN_ERROR', '-o', '$TARGET', '-c', '$SOURCE']])
-
-obj_builder = Builder(source_scanner=ocamldep_scanner,
-                      emitter=obj_emitter,
-                      action=obj_action,
-                      suffix=obj_suffix,
-                      src_suffix=['.mli', '.ml'],
-                      single_source=True,
-                      )
+__ocamldep_scanner = Scanner(
+    function=__ocamldep_scan,
+    name='ocamldep_scanner',
+    node_class=File,
+    skeys=['.mli', '.ml'],
+    )
 
 
 ########################################################################
 
 
-def lib_emitter(target, source, env):
-    #suffix = { False: '.cmo', True: '.cmx' }[env['OCAML_NATIVE']]
-    #source = [ node for node in source if node.get_suffix() == suffix ]
+def __obj_emitter(target, source, env):
+    if env['OCAML_DTYPES'] or env['OCAML_NATIVE']:
+        cmo_suffix = __source_to_object[env['OCAML_NATIVE']]['.ml']
+        cmos = ( node for node in target if node.get_suffix() == cmo_suffix )
+        for cmo in cmos:
+            if env['OCAML_DTYPES']:
+                target.append(cmo.target_from_source('', '.annot'))
+            if env['OCAML_NATIVE']:
+                target.append(node.target_from_source('', '.o'))
+    return target, source
+
+
+def __obj_suffix(env, sources):
+    [source] = sources
+    result = __source_to_object[env['OCAML_NATIVE']][source.get_suffix()]
+    return result
+
+
+__obj_action = Action([['$_OCAMLC', '$_OCAML_DEBUG', '$_OCAML_DTYPES', '$_OCAML_PATH', '$_OCAML_PP', '$_OCAML_WARN', '$_OCAML_WARN_ERROR', '-o', '$TARGET', '-c', '$SOURCE']])
+
+
+__obj_builder = Builder(
+    source_scanner=__ocamldep_scanner,
+    emitter=__obj_emitter,
+    action=__obj_action,
+    suffix=__obj_suffix,
+    src_suffix=['.mli', '.ml'],
+    single_source=True,
+    )
+
+
+########################################################################
+#
+#  Library builder
+#
+#  Building a library is fairly straightforward: run a command on a
+#  list of sources to produce the target.  The only subtlety is that
+#  native libraries have an extra depepdency: the ".a" file for each
+#  ".cmxa" file.  We tell SCons about that using an emitter function.
+#
+
+
+__lib_suffix = { False: '.cma', True: '.cmxa' }
+
+
+def __lib_emitter(target, source, env):
     if env['OCAML_NATIVE']:
         afiles = ( node.target_from_source('', '.a') for node in target )
         target += afiles
     return target, source
 
 
-lib_action = Action([['$_OCAMLC', '-a', '$_OCAML_WARN', '$_OCAML_WARN_ERROR', '-o', '$TARGET', '$SOURCES']])
+__lib_action = Action([['$_OCAMLC', '-a', '$_OCAML_WARN', '$_OCAML_WARN_ERROR', '-o', '$TARGET', '$SOURCES']])
 
-lib_builder_bytecode = Builder(emitter=lib_emitter,
-                               action=lib_action,
-                               suffix='.cma',
-                               src_suffix='.cmo',
-                               src_builder=obj_builder,
-                               )
 
-lib_builder_native = Builder(emitter=lib_emitter,
-                             action=lib_action,
-                             suffix='.cmxa',
-                             src_suffix='.cmx',
-                             src_builder=obj_builder,
-                             )
-
-lib_builder = { False: lib_builder_bytecode,
-                True:  lib_builder_native }
+def __lib_builder(native):
+    return Builder(
+        emitter=__lib_emitter,
+        action=__lib_action,
+        src_builder=__obj_builder,
+        src_suffix=__source_to_object[native]['.ml'],
+        suffix=__lib_suffix[native]
+        )
 
 
 ########################################################################
 
 
-def link_depends(node, env):
+def __exe_depends(node, env):
     if node.get_suffix() == '.o': return []
     obj_suffix = node.get_suffix()
-    src_suffix = object_to_source[obj_suffix]
+    src_suffix = __object_to_source[obj_suffix]
     source = node.target_from_source('', src_suffix)
     cmis = ( cmi for cmi in node.children() if cmi != source )
     cmos = ( cmi.target_from_source('', obj_suffix) for cmi in cmis )
     cmos = ( env.FindFile(cmo, '#.') for cmo in cmos )
     cmos = filter(None, cmos)
     if env['OCAML_NATIVE']:
-        cmos = list(cmos)
-        objs = [ cmo.target_from_source('', '.o') for cmo in cmos ]
-        return cmos + objs
+        deps = ( (cmo, cmo.target_from_source('', '.o')) for cmo in cmos )
+        return chain(*deps)
     else:
         return cmos
 
-def link_scanner(node, env, path):
-    __pychecker__ = 'no-argsused'
-    return list(link_depends(node, env))
 
-exe_scanner = Scanner(function=link_scanner, name='exe_scanner',
-                      path_function=ocaml_path_function,
-                      node_class=File, recursive=True)
+def __exe_scan(node, env, path):
+    __pychecker__ = 'no-argsused'
+    return __exe_depends(node, env)
+
+
+__exe_scanner = Scanner(
+    function=__exe_scan,
+    name='exe_scanner',
+    node_class=File,
+    recursive=True,
+    )
 
 
 def __find_libs(libs, env, path):
@@ -183,63 +187,48 @@ def __find_libs(libs, env, path):
         if found:
             yield found
         else:
-            warn('no %s in search path [%s]' % (lib, ', '.join(path)))
+            __warn('no %s in search path [%s]' % (lib, ', '.join(path)))
 
 
-def __exe_target_scanner(node, env, path):
+def __exe_target_scan(node, env, path):
     __pychecker__ = 'no-argsused'
-    suffix = { False: '.cma', True: '.cmxa' }[env['OCAML_NATIVE']]
+    suffix = __lib_suffix[env['OCAML_NATIVE']]
     libs = ( lib + suffix for lib in env['OCAML_LIBS'] )
     libs = __find_libs(libs, env, path)
-    libs = list(libs)
     return libs
 
-exe_target_scanner = Scanner(function=__exe_target_scanner,
-                             name='exe_target_scanner',
-                             path_function=ocaml_path_function,
-                             node_class=File)
 
-
-def link_order_expand(cmo, env, seen):
-    order = []
-    obj_suffix = cmo.get_suffix()
-    if not cmo in seen:
-        prereqs = link_depends(cmo, env)
-        prereqs = ( pre.target_from_source('', obj_suffix) for pre in prereqs )
-        prereqs = ( pre for pre in prereqs if pre != cmo )
-        prereqs = ( link_order_expand(pre, env, seen) for pre in prereqs )
-        prereqs = chain(*prereqs)
-        order += prereqs
-        order.append(cmo)
-        seen.add(cmo)
-    return order
-
-def __var_ocaml_link_order(target, source, env, for_signature):
+def __exe_path_function(env, node):
     __pychecker__ = 'no-argsused'
-    order = link_order_expand(source[0], env, set([]))
-    assert len(order) == len(set(order)), 'for target %s: duplicates in link order: %s' % (target[0], strs(order))
-    return order
+    stdlib = env['OCAML_STDLIB']
+    if stdlib is None:
+        ocamlc = env['OCAMLC']
+        ocamlc = env.WhereIs(ocamlc)
+        prefix = dirname(dirname(ocamlc))
+        stdlib = '%s/lib/ocaml' % prefix
+
+    return tuple(env['OCAML_PATH'] + [stdlib])
 
 
-exe_action = Action([['$_OCAMLC', '$_OCAML_PATH', '-o', '$TARGET', '$_OCAML_LIBS', '$_OCAML_LINK_ORDER']])
+__exe_target_scanner = Scanner(
+    function=__exe_target_scan,
+    name='exe_target_scanner',
+    path_function=__exe_path_function,
+    node_class=File,
+    )
 
 
-exe_builder_bytecode = Builder(target_scanner=exe_target_scanner,
-                               source_scanner=exe_scanner,
-                               action=exe_action,
-                               src_suffix='.cmo',
-                               src_builder=obj_builder,
-                               )
+__exe_action = Action([['$_OCAMLC', '$_OCAML_PATH', '-o', '$TARGET', '$_OCAML_LIBS', '$_OCAML_LINK_ORDER']])
 
-exe_builder_native = Builder(target_scanner=exe_target_scanner,
-                             source_scanner=exe_scanner,
-                             action=exe_action,
-                             src_suffix='.cmx',
-                             src_builder=obj_builder,
-                             )
 
-exe_builder = { False: exe_builder_bytecode,
-                True:  exe_builder_native }
+def __exe_builder(native):
+    return Builder(
+        target_scanner=__exe_target_scanner,
+        source_scanner=__exe_scanner,
+        action=__exe_action,
+        src_suffix=__source_to_object[native]['.ml'],
+        src_builder=__obj_builder,
+        )
 
 
 ########################################################################
@@ -247,34 +236,60 @@ exe_builder = { False: exe_builder_bytecode,
 
 def __var_ocamlc(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
-    if env['OCAML_NATIVE']:
-        return env['OCAMLOPT']
-    else:
-        return env['OCAMLC']
+    slots = {False: 'OCAMLC', True: 'OCAMLOPT'}
+    slot = slots[env['OCAML_NATIVE']]
+    return env[slot]
+
 
 def __var_ocaml_cma(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
-    return { False: '.cma', True: '.cmxa' }[env['OCAML_NATIVE']]
+    return __lib_suffix[env['OCAML_NATIVE']]
+
 
 def __var_ocaml_debug(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
     if env['OCAML_DEBUG']:
         return '-g'
 
+
 def __var_ocaml_dtypes(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
     if env['OCAML_DTYPES']:
         return ['$(', '-dtypes', '$)']
+
+
+def __var_ocaml_link_order(target, source, env, for_signature):
+    __pychecker__ = 'no-argsused'
+
+    def __link_order_expand(cmo, env, seen):
+        order = []
+        obj_suffix = cmo.get_suffix()
+        if not cmo in seen:
+            prereqs = __exe_depends(cmo, env)
+            prereqs = ( pre.target_from_source('', obj_suffix) for pre in prereqs )
+            prereqs = ( pre for pre in prereqs if pre != cmo )
+            prereqs = ( __link_order_expand(pre, env, seen) for pre in prereqs )
+            prereqs = chain(*prereqs)
+            order += prereqs
+            order.append(cmo)
+            seen.add(cmo)
+        return order
+
+    [source] = source
+    return __link_order_expand(source, env, set([]))
+
 
 def __var_ocaml_pp(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
     if env['OCAML_PP']:
         return ['-pp', env['OCAML_PP']]
 
+
 def __var_ocaml_warn(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
     if env['OCAML_WARN']:
         return ['$(', '-w', env['OCAML_WARN'], '$)']
+
 
 def __var_ocaml_warn_error(target, source, env, for_signature):
     __pychecker__ = 'no-argsused'
@@ -307,15 +322,15 @@ def generate(env):
         _OCAML_WARN=__var_ocaml_warn,
         _OCAML_WARN_ERROR=__var_ocaml_warn_error,
 
-        SCANNERS=[ocamldep_scanner],
+        SCANNERS=[__ocamldep_scanner],
 
         BUILDERS={
-        'OcamlObject': obj_builder,
-        'OcamlLibrary': lib_builder[env['OCAML_NATIVE']],
-        'OcamlProgram': exe_builder[env['OCAML_NATIVE']],
+        'OcamlObject': __obj_builder,
+        'OcamlLibrary': __lib_builder(env['OCAML_NATIVE']),
+        'OcamlProgram': __exe_builder(env['OCAML_NATIVE']),
         },
         )
 
 
 def exists(env):
-    return env.Detect(['ocamlc.opt', 'ocamlc'])
+    return env.Detect(['ocamlc', 'ocamlc.opt', 'ocamlopt', 'ocamlopt.opt'])

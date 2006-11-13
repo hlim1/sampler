3ccc969db2015af40bedf351cb05594a9dbf35c4
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:07:30 +0000

    Semi-automated mass renaming of all release tags.

---
+++ sampler/1.4/1/launcher/tray/PopupMenu.py
@@ -0,0 +1,40 @@
+from LazyWidget import LazyWidget
+
+
+########################################################################
+
+
+class PopupMenu(LazyWidget):
+
+    __slots__ = ['__about', '__client', '__master', '__notifier']
+
+    def __init__(self, client):
+        from AboutDialog import AboutDialog
+        LazyWidget.__init__(self, 'popup')
+        self.__about = AboutDialog(client)
+        self.__client = client
+
+    def populate(self, xml, widget):
+        __pychecker__ = 'no-argsused'
+        from MasterNotifier import MasterNotifier
+        self.__master = xml.get_widget('menu-master')
+        self.__notifier = MasterNotifier(self.__client, self.__master.set_active)
+
+    def popup(self, event):
+        import gtk.gdk
+        assert event.type == gtk.gdk.BUTTON_PRESS
+        self.widget().popup(None, None, None, event.button, event.time)
+
+    def on_master_toggled(self, item):
+        import Keys
+        active = item.get_active()
+        self.__client.set_bool(Keys.master, active)
+
+    def on_preferences_activate(self, item):
+        __pychecker__ = 'no-argsused'
+        import PreferencesDialog
+        PreferencesDialog.present()
+
+    def on_about_activate(self, item):
+        __pychecker__ = 'no-argsused'
+        self.__about.present()

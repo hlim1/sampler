6797b2efa11fd52473c0e97390be14ab3f7b98db
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:27 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gaim/1.4.0/4.sam.2/lib/schemes/function-entries.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_sampler_function_entries_h
+#define INCLUDE_sampler_function_entries_h
+
+#include "../signature.h"
+#include "tuple-1.h"
+
+
+void functionEntriesReport(const SamplerUnitSignature, unsigned, const SamplerTuple1 []);
+
+
+#endif /* !INCLUDE_sampler_function_entries_h */

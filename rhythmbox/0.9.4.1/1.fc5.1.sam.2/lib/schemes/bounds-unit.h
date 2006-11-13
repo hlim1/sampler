4763438a803a18eb0f0fd77bdbc3c51b8f393ec4
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:07:13 +0000

    Semi-automated mass renaming of all release tags.

---
+++ rhythmbox/0.9.4.1/1.fc5.1.sam.2/lib/schemes/bounds-unit.h
@@ -0,0 +1,45 @@
+#ifndef INCLUDE_sampler_bounds_unit_h
+#define INCLUDE_sampler_bounds_unit_h
+
+#include "../unit-signature.h"
+#include "bounds.h"
+
+
+#ifdef SAMPLER_TIMESTAMP_FIRST
+#pragma cilnoremove("boundsTimestampsFirst");
+static samplerTimestamp boundsTimestampsFirst[];
+#endif /* SAMPLER_TIMESTAMP_FIRST */
+
+#ifdef SAMPLER_TIMESTAMP_LAST
+#pragma cilnoremove("boundsTimestampsLast");
+static samplerTimestamp boundsTimestampsLast[];
+#endif /* SAMPLER_TIMESTAMP_LAST */
+
+
+#pragma cilnoremove("boundsReportDump")
+#pragma sampler_exclude_function("boundsReportDump")
+static void boundsReportDump();
+
+
+#pragma cilnoremove("boundsReporter")
+#pragma sampler_exclude_function("boundsReporter")
+static void boundsReporter() __attribute__((unused))
+{
+  boundsReportBegin(samplerUnitSignature);
+  boundsReportDump();
+  boundsReportEnd();
+
+#ifdef SAMPLER_TIMESTAMP_FIRST
+  timestampsReport(samplerUnitSignature, "bounds", "first",
+		   sizeof(boundsTimestampsFirst) / sizeof(*boundsTimestampsFirst),
+		   boundsTimestampsFirst);
+#endif /* SAMPLER_TIMESTAMP_FIRST */
+#ifdef SAMPLER_TIMESTAMP_LAST
+  timestampsReport(samplerUnitSignature, "bounds", "last",
+		   sizeof(boundsTimestampsLast) / sizeof(*boundsTimestampsLast),
+		   boundsTimestampsLast);
+#endif /* SAMPLER_TIMESTAMP_LAST */
+}
+
+
+#endif /* !INCLUDE_sampler_bounds_unit_h */
---
+++ rhythmbox/0.9.4.1/1.fc5.1.sam.2/lib/schemes/tuple-9.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_sampelr_tuple_9_h
+#define INCLUDE_sampelr_tuple_9_h
+
+
+typedef unsigned SamplerTuple9[9];
+
+void samplesDump9(unsigned, const SamplerTuple9 []);
+
+
+#endif /* !INCLUDE_sampelr_tuple_9_h */

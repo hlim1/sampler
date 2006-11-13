72afa55bfc3113fc1a94c4095b1a8f9bd845d949
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:42 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gimp/2.2.12/1.fc5.sam.1/instrumentor/branches/sharing.c
@@ -0,0 +1,35 @@
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "library.h"
+
+
+static void dlcheck()
+{
+  const char * const message = dlerror();
+  if (message)
+    {
+      fputs(message, stderr);
+      fputc('\n', stderr);
+      exit(3);
+    }
+}
+
+
+int main()
+{
+  void * const plugin = dlopen("./plugin.so", RTLD_LAZY);
+  dlcheck();
+
+  {
+    void (* const function)(int) = (void (*)(int)) dlsym(plugin, "function");
+    dlcheck();
+    function(1);
+  }
+  
+  libraryFunction(1);
+
+  dlclose(plugin);
+  dlcheck();
+  return 0;
+}

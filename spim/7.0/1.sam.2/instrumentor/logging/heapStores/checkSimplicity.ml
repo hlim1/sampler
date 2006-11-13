cd7dbd50740d894a8d1d842fab0196e2f8cbcbe8
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:07:35 +0000

    Semi-automated mass renaming of all release tags.

---
+++ spim/7.0/1.sam.2/instrumentor/logging/heapStores/checkSimplicity.ml
@@ -0,0 +1,48 @@
+open Cil
+
+
+let bad message printer thing =
+  try ignore(error message printer thing)
+  with Errormsg.Error -> ()
+    
+    
+let checkLvalue = function
+  | (Mem Lval (Var _, NoOffset), NoOffset) -> ()
+  | (Mem _, offset) when BitFields.present offset -> ()
+  | (Var _, _) -> ()
+  | other -> bad "complex lvalue: %a" d_lval other
+	
+	
+class visitor =
+  object
+    inherit FunctionBodyVisitor.visitor
+
+    method vstmt _ = DoChildren
+
+    method vinst inst =
+      begin
+	match inst with
+	| Set (lval, rval, _) ->
+	    begin
+	      checkLvalue lval;
+	      match lval with
+	      | (Mem _, _) ->
+		  begin
+		    match rval with
+		    | Lval (Var _, NoOffset) -> ()
+		    | other -> bad "complex rvalue: %a" d_exp other
+		  end
+	      | _ -> ()
+	    end
+	| Call (Some lval, _, _, _) ->
+	    checkLvalue lval
+	| _ -> ()
+      end;
+      
+      SkipChildren
+  end
+
+
+let phase =
+  "CheckSimplicity",
+  visitCilFileSameGlobals new visitor

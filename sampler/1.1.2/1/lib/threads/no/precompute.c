9a32e237d4e58d2a3b82200fa84700000794ef18
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:07:27 +0000

    Semi-automated mass renaming of all release tags.

---
+++ sampler/1.1.2/1/lib/threads/no/precompute.c
@@ -0,0 +1,52 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../random-online.h"
+#include "../random-offline-size.h"
+
+
+int nextEventCountdown;
+
+
+char *envSparsity;
+char *envSeed;
+
+
+static void writeCountdown(FILE *outfile, int value)
+{
+  fwrite(&value, sizeof(value), 1, outfile);
+}
+
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+    {
+      fprintf(stderr, "Usage: %s <outfile>\n", argv[0]);
+      exit(2);
+    }
+  else
+    {
+      const char * const filename = argv[1];
+      FILE * const outfile = fopen(filename, "w");
+      if (!outfile)
+	{
+	  fprintf(stderr, "precompute: cannot open \"%s\" for writing: %s\n", filename, strerror(errno));
+	  exit(1);
+	}
+      else
+	{
+	  int slot = PRECOMPUTE_COUNT;
+	  while (slot--)
+	    {
+	      writeCountdown(outfile, nextEventCountdown);
+	      nextEventCountdown = getNextEventCountdown();
+	    }
+
+	  fclose(outfile);
+	}
+    }
+
+  return 0;
+}

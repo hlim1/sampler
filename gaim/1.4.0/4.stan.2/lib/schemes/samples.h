22cccc3aab12c25945308f7366f199d73fb7eb53
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:27 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gaim/1.4.0/4.stan.2/lib/schemes/samples.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_sampler_samples_h
+#define INCLUDE_sampler_samples_h
+
+#include "../signature.h"
+
+
+void samplesBegin(const SamplerUnitSignature, const char []);
+void samplesEnd();
+
+
+#endif /* !INCLUDE_sampler_samples_h */

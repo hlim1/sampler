e21dfffc3c1655cf3250d2dabc9dc2abdfc3b1ba
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:07 +0000

    Semi-automated mass renaming of all release tags.

---
+++ evolution/2.2.3/2.fc4.sam.2/launcher/common/BlipIcons.py
@@ -0,0 +1,41 @@
+import gtk
+
+
+########################################################################
+
+
+def __source(abled, size):
+    import Config
+    import os.path
+
+    source = gtk.IconSource()
+    filename = abled + '-' + str(size) + '.png'
+    source.set_filename(os.path.join(Config.pixmapsdir, filename))
+
+    return source
+
+
+def __install(abled):
+    set = gtk.IconSet()
+
+    source_48 = __source(abled, 48)
+    source_48.set_size_wildcarded(False)
+    source_48.set_size(gtk.ICON_SIZE_DIALOG)
+    set.add_source(source_48)
+
+    source_96 = __source(abled, 96)
+    #source_96.set_size_wildcarded(False)
+    source_96.set_size(ICON_SIZE_EMBLEM)
+    set.add_source(source_96)
+
+    factory = gtk.IconFactory()
+    factory.add('sampler-' + abled, set)
+    factory.add_default()
+
+
+ICON_SIZE_EMBLEM = gtk.icon_size_register('sampler-emblem', 96, 96)
+
+__install('disabled')
+__install('enabled')
+
+stock = ['sampler-disabled', 'sampler-enabled']
---
+++ evolution/2.2.3/2.fc4.sam.2/launcher/common/LazyIcon.py
@@ -0,0 +1,15 @@
+class LazyIcon:
+    def __init__(self, filename):
+        self.__image = filename
+
+    def get(self):
+        import types
+        if isinstance(self.__image, types.StringTypes):
+            import gtk.gdk
+            import os.path
+            import Paths
+
+            fullpath = os.path.join(Paths.home, self.__image)
+            self.__image = gtk.gdk.pixbuf_new_from_file(fullpath)
+
+        return self.__image

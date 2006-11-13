7e58e4c5eb45acf8a66c5bc2eaa9eed52454a938
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:29 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gaim/1.5.0/1.stan.1/lib/schemes/tuple-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../report.h"
+#include "tuple-2.h"
+
+
+void samplesDump2(unsigned count, const SamplerTuple2 tuples[])
+{
+  unsigned scan;
+
+  for (scan = 0; scan < count; ++scan)
+    fprintf(reportFile, "%u\t%u\n",
+	    tuples[scan][0],
+	    tuples[scan][1]);
+}

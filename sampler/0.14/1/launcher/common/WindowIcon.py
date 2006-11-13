18a48368e71df39713b1d3fcc8c2ac9776fdbb3b
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:07:20 +0000

    Semi-automated mass renaming of all release tags.

---
+++ sampler/0.14/1/launcher/common/WindowIcon.py
@@ -0,0 +1,15 @@
+import gtk
+
+from MasterNotifier import MasterNotifier
+
+import BlipIcons
+
+
+class WindowIcon:
+    def __init__(self, client, widget):
+        self.__widget = widget
+        self.__notify = MasterNotifier(client, self.__enabled_refresh)
+
+    def __enabled_refresh(self, enabled):
+        pixbuf = self.__widget.render_icon(BlipIcons.stock[enabled], BlipIcons.ICON_SIZE_EMBLEM, "")
+        self.__widget.set_icon(pixbuf)
---
+++ sampler/0.14/1/lib/tuple-4.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_sampelr_tuple_4_h
+#define INCLUDE_sampelr_tuple_4_h
+
+
+typedef unsigned SamplerTuple4[4];
+
+void samplesDump4(unsigned, const SamplerTuple4 []);
+
+
+#endif /* !INCLUDE_sampelr_tuple_4_h */

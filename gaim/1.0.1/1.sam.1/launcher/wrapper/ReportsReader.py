d735b761a4e048bc2df14f12c54156c62e59371f
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:19 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gaim/1.0.1/1.sam.1/launcher/wrapper/ReportsReader.py
@@ -0,0 +1,26 @@
+import cStringIO
+import re
+
+
+########################################################################
+#
+#  Read report stream from instrumented application and split it apart
+#  into individual reports
+#
+
+class ReportsReader (dict):
+    def __init__(self, source):
+        startTag = re.compile('^<report id="([^"]+)">\n$')
+
+        for line in source:
+            match = startTag.match(line)
+            if match:
+                name = match.group(1)
+                if not name in self:
+                    self[name] = cStringIO.StringIO()
+
+                for line in source:
+                    if line == '</report>\n':
+                        break
+                    else:
+                        self[name].write(line)
---
+++ gaim/1.0.1/1.sam.1/mining/queries/outcome-summary.sql
@@ -0,0 +1,13 @@
+-- outcome of each applcation's runs, ordered by crash frequency
+
+SELECT
+    application_name,
+    count(*),
+    sum(!exit_status AND !exit_signal) AS good,
+    sum(!!exit_status) AS error,
+    sum(!!exit_signal) AS crash,
+    sum(!!exit_signal) / sum(!exit_status AND !exit_signal) AS crash_rate
+FROM run NATURAL JOIN build
+WHERE build_suppress IS NULL
+GROUP BY application_name
+ORDER BY crash_rate DESC

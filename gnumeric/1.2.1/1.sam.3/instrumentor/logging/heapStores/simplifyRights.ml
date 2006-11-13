6d9b44d6a79e2ebc54534024edabdcdce56f7543
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:49 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gnumeric/1.2.1/1.sam.3/instrumentor/logging/heapStores/simplifyRights.ml
@@ -0,0 +1,24 @@
+open Cil
+
+
+class visitor =
+  object(self)
+    inherit SimplifyVisitor.visitor
+	
+    method vinst = function
+      | Set ((Mem _, _) as lval, data, location) ->
+	  begin
+	    match data with
+	    | Lval (Var _, NoOffset) -> SkipChildren
+	    | _ ->
+		let temp = var (self#makeTempVar "right" (typeOf data)) in
+		ChangeTo [Set (temp, data, location);
+			  Set (lval, Lval temp, location)]
+	  end
+      | _ -> SkipChildren
+  end
+
+
+let phase =
+  "SimplifyRights",
+  visitCilFileSameGlobals new visitor
---
+++ gnumeric/1.2.1/1.sam.3/lib/scalar-pairs.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_libscalar_pairs_scalar_pairs_h
+#define INCLUDE_libscalar_pairs_scalar_pairs_h
+
+
+#pragma cilnoremove("scalarPairsCounters")
+static unsigned scalarPairsCounters[][3];
+
+
+#endif /* !INCLUDE_libscalar_pairs_scalar_pairs_h */

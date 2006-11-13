16bcb5dfc770a0b38ae2a156a17df39253f07432
Ben Liblit <liblit@cs.wisc.edu>
2006-11-13 07:06:15 +0000

    Semi-automated mass renaming of all release tags.

---
+++ gaim/0.77/0.sam.1/lib/branches-types.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_sampler_branches_types_h
+#define INCLUDE_sampler_branches_types_h
+
+
+typedef unsigned BranchTuple[2];
+
+
+void branchesReport(const unsigned char *, unsigned, const BranchTuple []);
+
+
+#endif /* !INCLUDE_sampler_branches_types_h */
---
+++ gaim/0.77/0.sam.1/lib/returns-types.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_sampler_returns_types_h
+#define INCLUDE_sampler_returns_types_h
+
+
+typedef unsigned ReturnTuple[3];
+
+
+void returnsReport(const unsigned char *, unsigned, const ReturnTuple []);
+
+
+#endif /* !INCLUDE_sampler_returns_types_h */

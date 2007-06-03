c014867d5529a4c5c635a7f7615d458a4492bc79
Ben Liblit <liblit@cs.wisc.edu>
2007-06-03 16:47:04 +0000

    nautilus-2.18.1-2.fc7.sam.1

---
+++ nautilus/2.18.1/2.fc7.sam.1/launcher/wrapper/Main2.py
@@ -0,0 +1,54 @@
+import sys
+import Paths
+sys.path.append(Paths.common)
+
+import pygtk
+pygtk.require('2.0')
+
+import ORBit
+ORBit.load_typelib('Everything')
+
+
+########################################################################
+
+
+def main(name, wrapped, upload_headers, **extras):
+    __pychecker__ = 'no-argsused'
+    from AppConfig import AppConfig
+    from UserConfig import UserConfig
+
+    app = AppConfig(name, wrapped, upload_headers)
+    user = UserConfig(name)
+
+    sparsity = user.sparsity()
+    if sparsity > 0:
+        import SampledLauncher
+        launcher = SampledLauncher.SampledLauncher(app, user, sparsity)
+    else:
+        import UnsampledLauncher
+        launcher = UnsampledLauncher.UnsampledLauncher(app)
+
+    launcher.spawn()
+
+    if not user.asked():
+        from subprocess import Popen
+        Popen([Paths.first_time + '/first-time'])
+
+    from bonobo.activation import activate
+    try:
+        monitor = activate("iid == 'OAFIID:SamplerMonitor:0.1'")
+    except RuntimeError:
+        monitor = None
+
+    try:
+        outcome = launcher.wait()
+
+    finally:
+        if monitor != None:
+            import CORBA
+            try:
+                monitor.unref()
+            except CORBA.COMM_FAILURE:
+                pass
+
+    outcome.exit()

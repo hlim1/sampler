c4db938d4cbc73e4780066aac1a4014f9ce2f0fe
Ben Liblit <liblit@cs.wisc.edu>
2007-08-09 07:36:31 +0000

    evolution-2.10.3-2.fc7.sam.1

---
+++ evolution/2.10.3/2.fc7.sam.1/trunk/launcher/tray/AboutDialog.py
@@ -0,0 +1,23 @@
+from LazyDialog import LazyDialog
+
+
+class AboutDialog(LazyDialog):
+
+    __slots__ = ['__notifier']
+
+    def __init__(self, client):
+        from MasterNotifier import MasterNotifier
+        LazyDialog.__init__(self, client, 'about')
+        self.__notifier = MasterNotifier(client, self.__enabled_refresh)
+
+    def populate(self, xml, widget):
+        import SamplerConfig
+        LazyDialog.populate(self, xml, widget)
+        widget.set_property('name', 'Bug Isolation Monitor')
+        widget.set_property('version', SamplerConfig.version)
+
+    def __enabled_refresh(self, enabled):
+        import BlipIcons
+        widget = self.widget()
+        pixbuf = widget.render_icon(BlipIcons.stock[enabled], BlipIcons.ICON_SIZE_EMBLEM, "")
+        widget.set_property('logo', pixbuf)

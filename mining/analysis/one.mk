f23738cedafceb00fd05e399d20bfe9303d2f1b0
Ben Liblit <liblit@cs.wisc.edu>
2007-12-10 12:50:16 +0000

    If we see an "alien" compilation unit not from the current package, warn but keep processing the rest of the data.  This helps at least one package work that didn't work before.  Not sure how many others are affected.
    NOTE: requires cbiexp r1902 or later!

--- mining/analysis/one.mk
+++ mining/analysis/one.mk
@@ -6,6 +6,7 @@ srcdir ?= debug/usr/src/debug
 
 sites ?= $(shell find sites -name '*.sites')
 source-strip-prefixes ?= /usr/src/debug/
+convert_reports_flags ?= --ignore-alien-units
 update-tools ?=
 
 include $(root)/src/analysis-rules.mk

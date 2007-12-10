fd2f8d2f055a08b95f9e6686dfb2950edfa120dd
Ben Liblit <liblit@cs.wisc.edu>
2007-12-10 12:14:56 +0000

    Restore "/afs/cs.wisc.edu/p/cbi/public/build/cbiexp" as the default directory for the root of the CBI analysis tools tree.  However, this can be overridden by creating a "sampler/mining/analysis/local.mk" file like this:
         root = /u/f/l/fletchal/CBI/cbiexp/trunk
    
    If that "local.mk" file is missing, we quietly fall back on
    "/afs/cs.wisc.edu/p/cbi/public/build/cbiexp" as the default.
    
    Subversion has been told to ignore "local.mk", so this is a good place
    to put customizations that are not appropriate as permanent additions
    to the source tree.

--- mining/analysis/one.mk
+++ mining/analysis/one.mk
@@ -1,11 +1,12 @@
-#root := /afs/cs.wisc.edu/p/cbi/public/build/cbiexp
-root := /u/f/l/fletchal/CBI/cbiexp/trunk
-datadir := data
-srcdir := debug/usr/src/debug
+include ../../../local.mk
+
+root ?= /afs/cs.wisc.edu/p/cbi/public/build/cbiexp
+datadir ?= data
+srcdir ?= debug/usr/src/debug
 
 sites ?= $(shell find sites -name '*.sites')
-source-strip-prefixes = /usr/src/debug/
-update-tools =
+source-strip-prefixes ?= /usr/src/debug/
+update-tools ?=
 
 include $(root)/src/analysis-rules.mk
 

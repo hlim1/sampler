65b9ae6fa97d483087c3ca680f59fc9315daf7f7
Ben Liblit <liblit@cs.wisc.edu>
2007-12-11 03:47:43 +0000

    Oops, remove a left-over bit of debugging cruft.

--- mining/analysis/stamps.sql
+++ mining/analysis/stamps.sql
@@ -3,8 +3,7 @@ FROM run
   NATURAL JOIN build
   LEFT JOIN run_suppress USING (run_id)
   LEFT JOIN build_suppress USING (build_id)
-WHERE exit_signal = 0
-  AND run_suppress.reason IS NULL
+WHERE run_suppress.reason IS NULL
   AND build_suppress.reason IS NULL
 GROUP BY application_name, application_version, application_release, build_distribution
   HAVING COUNT(NULLIF(exit_signal = 0, FALSE)) > 1

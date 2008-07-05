dd07650426c59357736b0f84b17e1edcce40cca9
Ben Liblit <liblit@cs.wisc.edu>
2008-07-05 21:08:08 +0000

    pidgin-2.4.3-1.fc9.sam.1.i386

---
+++ pidgin/2.4.3/1.fc9.sam.1/lib/threads/threads.mk
@@ -0,0 +1,12 @@
+headers =					\
+	lock.h					\
+	once.h
+
+common =					\
+	hijack.c				\
+	../countdown.c				\
+	../initialize.c				\
+	../registry.c				\
+	../report.c				\
+	../timestamps-set.c			\
+	../verbose.c

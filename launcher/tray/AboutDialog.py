fc48f32bb1261efc4b12e40d86058b8322248f24
Ben Liblit <liblit@cs.wisc.edu>
2009-09-20 06:44:20 +0000

    Let GtkBuilder create the status icon for us.  Consolidate signal (event) handler methods into a single class so that GtkBuilder can wire them up to the UI automatically.

--- launcher/tray/AboutDialog.py
+++ launcher/tray/AboutDialog.py
@@ -10,23 +10,3 @@ class AboutDialog(object):
         self.__widget = builder.get_object('about')
         self.__icon_updater = WindowIcon(client, self.__widget)
         self.__notifier = MasterNotifier(client, self.__enabled_refresh)
-
-    def __enabled_refresh(self, enabled):
-        import BlipIcons
-        widget = self.__widget
-        pixbuf = widget.render_icon(BlipIcons.stock[enabled], BlipIcons.ICON_SIZE_EMBLEM, "")
-        widget.set_property('logo', pixbuf)
-
-    def present(self):
-        self.__widget.present()
-
-    def on_about_delete(self, dialog, event):
-        return True
-
-    def on_about_response(self, dialog, response):
-        if response < 0:
-            dialog.hide()
-            dialog.emit_stop_by_name('response')
-            return True
-        else:
-            return False

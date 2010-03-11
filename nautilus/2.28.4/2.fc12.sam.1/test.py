e6eff01fb3e025bed3c177eaec5b0afad3f0f7b1
Ben Liblit <liblit@cs.wisc.edu>
2010-03-11 18:07:39 +0000

    nautilus-2.28.4-2.fc12.sam.1.i686

---
+++ nautilus/2.28.4/2.fc12.sam.1/test.py
@@ -0,0 +1,31 @@
+import sys
+sys.path[1:1] = ['/usr/lib/scons']
+from SCons.Builder import Builder
+from SCons.Defaults import Touch
+
+
+def test_suffix(env, sources):
+    return sources[0].get_suffix() + '.passed'
+
+def TestBuilder(command, **kwargs):
+    return Builder(action=[command, Touch('$TARGET')], suffix=test_suffix, **kwargs)
+
+test_desktop_builder = TestBuilder(['desktop-file-validate', '$SOURCE'], single_source=True)
+test_python_builder = TestBuilder(['$pychecker', '--stdlib', '--quiet', '$SOURCES'])
+
+
+def generate(env):
+    env.AppendUnique(
+        BUILDERS={
+        'TestDesktop': test_desktop_builder,
+        'TestPython': test_python_builder,
+        },
+        )
+
+    env.SetDefault(
+        pychecker='pychecker',
+        )
+
+
+def exists(env):
+    return True

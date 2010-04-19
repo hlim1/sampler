ca95372bcea9c76b16e8c4251077a0a40c6e088c
Ben Liblit <liblit@cs.wisc.edu>
2010-04-19 20:14:52 +0000

    Avoid a warning from gcc-4.4.3 about an undefined macro that we do not actually evaluate anyway.

--- lib/schemes/tuple-bits.h
+++ lib/schemes/tuple-bits.h
@@ -4,23 +4,25 @@
 
 #ifndef CBI_TUPLE_COUNTER_BITS
 typedef unsigned cbi_TupleCounter;
-#define CBI_TUPLE_COUNTER_FORMAT "u"
-#define CBI_TUPLE_COUNTER_X86_INC_OPERAND_SUFFIX "l"
-
-#elif CBI_TUPLE_COUNTER_BITS == 32
-#include <inttypes.h>
-typedef uint32_t cbi_TupleCounter;
-#define CBI_TUPLE_COUNTER_FORMAT PRIu32
-#define CBI_TUPLE_COUNTER_X86_INC_OPERAND_SUFFIX "l"
-
-#elif CBI_TUPLE_COUNTER_BITS == 64
-#include <inttypes.h>
-typedef uint64_t cbi_TupleCounter;
-#define CBI_TUPLE_COUNTER_FORMAT PRIu64
-#define CBI_TUPLE_COUNTER_X86_INC_OPERAND_SUFFIX "q"
+#  define CBI_TUPLE_COUNTER_FORMAT "u"
+#  define CBI_TUPLE_COUNTER_X86_INC_OPERAND_SUFFIX "l"
 
 #else
-#error "unexpected value for CBI_TUPLE_COUNTER_BITS; should defined as 32 or 64 or left undefined"
+#  if CBI_TUPLE_COUNTER_BITS == 32
+#    include <inttypes.h>
+typedef uint32_t cbi_TupleCounter;
+#    define CBI_TUPLE_COUNTER_FORMAT PRIu32
+#    define CBI_TUPLE_COUNTER_X86_INC_OPERAND_SUFFIX "l"
+
+#  elif CBI_TUPLE_COUNTER_BITS == 64
+#    include <inttypes.h>
+typedef uint64_t cbi_TupleCounter;
+#    define CBI_TUPLE_COUNTER_FORMAT PRIu64
+#    define CBI_TUPLE_COUNTER_X86_INC_OPERAND_SUFFIX "q"
+
+#  else
+#    error "unexpected value for CBI_TUPLE_COUNTER_BITS; should defined as 32 or 64 or left undefined"
+#  endif
 #endif
 
 

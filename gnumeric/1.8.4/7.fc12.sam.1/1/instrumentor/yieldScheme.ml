e06f51916bebc85b721fb51da22c8ed779c240ea
Ben Liblit <liblit@cs.wisc.edu>
2010-09-14 18:05:03 +0000

    gnumeric-1.8.4-7.fc12.sam.1.i686

---
+++ gnumeric/1.8.4/7.fc12.sam.1/1/instrumentor/yieldScheme.ml
@@ -0,0 +1,27 @@
+open SchemeName
+
+(*cci*)
+let name = {
+  flag = "yields";
+  prefix = "yields";
+  ident = "Yields";
+}
+
+
+class c file : Scheme.c =
+  object
+    val tuples = new Counters.manager name file 
+
+    method private findAllSites =
+      let finder = new YieldFinder.visitor file tuples in
+      Scanners.iterFuncs file
+	(fun func ->
+	  let finder = finder func in
+	  ignore (Cil.visitCilFunction finder func));
+        tuples#patch  
+
+    method saveSiteInfo = tuples#saveSiteInfo 
+  end
+
+
+let factory = SchemeFactory.build name new c

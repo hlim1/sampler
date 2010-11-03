f0eb464b893acc8ad01e92a379a90de22ae388ce
Ben Liblit <liblit@cs.wisc.edu>
2010-11-03 17:14:34 +0000

    nautilus-2.32.0-1.fc14.sam.1.i686

---
+++ nautilus/2.32.0/1.fc14.sam.1/instrumentor/locationFilter.ml
@@ -0,0 +1,50 @@
+open Cil
+open Clude
+open Pretty
+
+
+class filter =
+  object (self)
+    inherit [location] Clude.filter as super
+
+    method private matches template focus =
+      matchesOrWildcard (-1) template.line focus.line &&
+      matchesOrWildcard "*" template.file focus.file
+
+    method private format = function
+      | {file=file; line=(-1)} ->
+	  text file ++ text ":*"
+      | fileAndLine ->
+	  d_loc () fileAndLine
+
+    method private makeLocation file line =
+      {file=file; line=line; byte=(-1)}
+
+    method private fileToLocation file =
+      self#makeLocation file (-1)
+
+    method private addExcludeFile file =
+      self#addExclude (self#fileToLocation file)
+
+    method private addIncludeFile file =
+      self#addInclude (self#fileToLocation file)
+
+    method private fileAndLineToLocation fileAndLine =
+      Scanf.sscanf fileAndLine "%s@:%u%!" self#makeLocation
+
+    method private addExcludeLocation fileAndLine =
+      self#addExclude (self#fileAndLineToLocation fileAndLine)
+
+    method private addIncludeLocation fileAndLine =
+      self#addInclude (self#fileAndLineToLocation fileAndLine)
+
+    initializer
+      Options.push ("--exclude-file", Arg.String self#addExcludeFile, "");
+      Options.push ("--include-file", Arg.String self#addIncludeFile, "<file-name> instrument this file");
+      Options.push ("--exclude-location", Arg.String self#addExcludeLocation, "");
+      Options.push ("--include-location", Arg.String self#addIncludeLocation, "<file-name:line-number> instrument this location");
+      Idents.register ("FilterLocation", fun () -> self#formatPatterns)
+  end
+
+
+let filter = new filter

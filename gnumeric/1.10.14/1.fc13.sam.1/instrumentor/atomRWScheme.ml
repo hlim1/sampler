db5a79fb30eb5616dfc67d96add84c5e885f0e9e
Ben Liblit <liblit@cs.wisc.edu>
2011-04-11 18:56:21 +0000

    gnumeric-1.10.14-1.fc13.sam.1.i686

---
+++ gnumeric/1.10.14/1.fc13.sam.1/instrumentor/atomRWScheme.ml
@@ -0,0 +1,29 @@
+open SchemeName
+open Cil
+
+(*cci*)
+let name = {
+  flag = "atoms-rw";
+  prefix = "atoms-rw";
+  ident = "Atoms-RW";
+}
+
+
+class c file : Scheme.c =
+  object
+    val tuples = new Counters.manager name file 
+
+    method private findAllSites =
+      let finder = new AtomFinder.visitor file tuples (* b_varinfo *) in
+      Scanners.iterFuncs file
+	(fun func ->
+	  let finder = finder func in
+	  ignore (Cil.visitCilFunction finder func  ));
+
+      tuples#patch  
+
+    method saveSiteInfo = tuples#saveSiteInfo 
+  end
+
+
+let factory = SchemeFactory.build name new c

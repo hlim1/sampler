219d443984a249da15691c8a287e8874d8155ae4
Ben Liblit <liblit@cs.wisc.edu>
2011-05-04 20:32:54 +0000

    Revert earlier switch to using list.insert instead of zero-length slice assignment.  The two are not exactly identical: list.insert takes a single item while zero-length slice assignment injects a list of several items.  The later is what we really need here.

--- launcher/preferences/AppFinder.py
+++ launcher/preferences/AppFinder.py
@@ -1,4 +1,4 @@
 class AppFinder(list):
     def __init__(self, client):
         import Keys
-        self.insert(0, client.all_dirs(Keys.applications))
+        self[0:0] = client.all_dirs(Keys.applications)

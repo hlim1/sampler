7a3ab3e5e8478709234d4a7d19f9d78d11991828
Ben Liblit <liblit@cs.wisc.edu>
2011-06-13 18:07:14 +0000

    gimp-2.6.11-14.fc14.sam.1.i686

---
+++ gimp/2.6.11/14.fc14.sam.1/scons-tools/expect.py
@@ -0,0 +1,57 @@
+from SCons.Script import *
+
+from itertools import chain, ifilter, imap
+
+import filecmp
+
+
+########################################################################
+#
+#  comparison with reference output
+#
+
+
+def __compare_action_exec(target, source, env):
+    __pychecker__ = 'no-argsused'
+    [actual, expected] = source
+    return not filecmp.cmp(str(actual), str(expected), False)
+
+
+def __compare_action_show(target, source, env):
+    __pychecker__ = 'no-argsused'
+    [actual, expected] = source
+    return 'compare "%s" and "%s"' % (actual, expected)
+
+
+__compare_action = Action(__compare_action_exec, __compare_action_show)
+
+
+__expect_action = [__compare_action, Touch('$TARGET')]
+
+
+def __expect_emitter(target, source, env):
+    [actual] = source
+    expected = actual.target_from_source('', '.expected')
+    return target, [actual, expected]
+
+
+__expect_builder = Builder(
+    action=__expect_action,
+    emitter=__expect_emitter,
+    suffix='.passed',
+    )
+
+
+########################################################################
+
+
+def generate(env):
+    env.AppendUnique(
+        BUILDERS={
+            'Expect': __expect_builder,
+            },
+        )
+
+
+def exists(env):
+    return True

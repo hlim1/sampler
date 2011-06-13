0f638b132f8b6ba4b4125847e788f43458791de3
Ben Liblit <liblit@cs.wisc.edu>
2011-06-13 01:39:35 +0000

    Remove some old debug output.

--- tools/cfg/memoize.ml
+++ tools/cfg/memoize.ml
@@ -2,11 +2,8 @@ let memoize worker =
   let known = new HashClass.c 0 in
   fun arg ->
     try
-      let result = known#find arg in
-      prerr_endline "remembered old result";
-      result
+      known#find arg
     with Not_found ->
       let result = worker arg in
       known#add arg result;
-      prerr_endline "memoized new result";
       result

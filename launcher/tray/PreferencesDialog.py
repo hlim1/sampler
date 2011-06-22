8736e58854c3e04833752fc55765e4b0563d4dde
Ben Liblit <liblit@cs.wisc.edu>
2011-06-22 22:22:19 +0000

    Launch the preferences dialog using D-Bus activation instead of running the "sampler-preferences" command via Popen.  Otherwise, the preferences process is killed (via SIGHUP) when the tray process exits.

--- launcher/tray/PreferencesDialog.py
+++ launcher/tray/PreferencesDialog.py
@@ -1,4 +1,8 @@
 def present():
-    from subprocess import Popen
-    import Paths
-    Popen([Paths.bin + '/sampler-preferences'])
+    import os
+    os.environ['DBUS_PYTHON_NO_DEPRECATED'] = '1'
+
+    import dbus
+    bus = dbus.SessionBus()
+    remote = bus.get_object('edu.wisc.cs.cbi.Preferences', '/edu/wisc/cs/cbi/Preferences')
+    remote.present()

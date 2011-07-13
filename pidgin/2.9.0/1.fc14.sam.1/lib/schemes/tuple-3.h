dfc9b66f8e2ce2c960c3e3502e63659c0c76fe83
Ben Liblit <liblit@cs.wisc.edu>
2011-07-13 17:03:58 +0000

    pidgin-2.9.0-1.fc14.sam.1.i686

---
+++ pidgin/2.9.0/1.fc14.sam.1/lib/schemes/tuple-3.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_sampelr_tuple_3_h
+#define INCLUDE_sampelr_tuple_3_h
+
+#include "tuple-bits.h"
+
+
+typedef cbi_TupleCounter cbi_Tuple3[3];
+
+void cbi_samplesDump3(unsigned, const cbi_Tuple3 []);
+
+
+#endif /* !INCLUDE_sampelr_tuple_3_h */

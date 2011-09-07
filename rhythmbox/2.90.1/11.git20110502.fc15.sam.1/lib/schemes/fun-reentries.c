7dd0bcbb17be1afe551d8f1634462f8a5be0b100
Ben Liblit <liblit@cs.wisc.edu>
2011-09-07 23:39:40 +0000

    rhythmbox-2.90.1-11.git20110502.fc15.sam.1.i686

---
+++ rhythmbox/2.90.1/11.git20110502.fc15.sam.1/lib/schemes/fun-reentries.c
@@ -0,0 +1,59 @@
+#include "fun-reentries.h"
+#include "samples.h"
+#include "tuple-2.h"
+
+static pthread_mutex_t funreLock __attribute__((unused)) = PTHREAD_MUTEX_INITIALIZER;
+
+void cbi_funReentriesReport(const cbi_UnitSignature signature,
+			       unsigned count, const cbi_Tuple2 counts[])
+{
+  cbi_samplesBegin(signature, "fun-reentries");
+  cbi_samplesDump2(count, counts);
+  cbi_samplesEnd();
+}
+
+
+void cbi_funre_lock()
+{
+  pthread_mutex_lock(&funreLock);
+}
+
+
+void cbi_funre_unlock()
+{
+  pthread_mutex_unlock(&funreLock);
+}
+
+void cci_atomicIncrementCounter(int *counter)
+{
+#if __i386__
+  asm ("lock incl %0"
+       : "+m" (*counter)
+       :
+       : "cc");
+#elif __x86_64__
+  asm ("lock incl %0"
+      : "=m" (*counter)
+      : "m" (*counter)
+      : "cc");
+#else
+#error "don't know how to atomically increment on this architecture"
+#endif
+}
+
+void cci_atomicDecrementCounter(int *counter)
+{
+#if __i386__
+  asm ("lock decl %0"
+       : "+m" (*counter)
+       :
+       : "cc");
+#elif __x86_64__
+  asm ("lock decl %0"
+      : "=m" (*counter)
+      : "m" (*counter)
+      : "cc");
+#else
+#error "don't know how to atomically increment on this architecture"
+#endif
+}

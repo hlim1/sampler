6b8526153770897346d697f5db85bdd0ec7eb336
Ben Liblit <liblit@cs.wisc.edu>
2011-09-21 18:23:20 +0000

    pidgin-2.10.0-1.fc14.sam.2.i686

---
+++ pidgin/2.10.0/1.fc14.sam.2/instrumentor/funcSiteInfo.ml
@@ -0,0 +1,11 @@
+open Cil
+open Pretty
+
+(*cci*)
+class c func inspiration =
+  object
+    inherit SiteInfo.c func inspiration as super
+
+    method print =
+      super#print @ [text func.svar.vname]
+  end

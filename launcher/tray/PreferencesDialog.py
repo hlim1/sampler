3f79cb23e28ef2b9ca2f8dd099da1c22bed70f98
Ben Liblit <liblit@cs.wisc.edu>
2011-09-23 22:03:28 +0000

    Use GIO-based D-Bus bindings for preferences dialog launching.

--- launcher/tray/PreferencesDialog.py
+++ launcher/tray/PreferencesDialog.py
@@ -1,8 +1,4 @@
 def present():
-    import os
-    os.environ['DBUS_PYTHON_NO_DEPRECATED'] = '1'
-
-    import dbus
-    bus = dbus.SessionBus()
-    remote = bus.get_object('edu.wisc.cs.cbi.Preferences', '/edu/wisc/cs/cbi/Preferences')
-    remote.present()
+    from gi.repository import Gio
+    preferences = Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION, Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES | Gio.DBusProxyFlags.DO_NOT_CONNECT_SIGNALS, None, 'edu.wisc.cs.cbi.Preferences', '/edu/wisc/cs/cbi/Preferences', 'org.gtk.Application', None)
+    preferences.Activate('(a{sv})', None)

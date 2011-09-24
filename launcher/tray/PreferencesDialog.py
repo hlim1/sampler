b33fc2843caedd79f171db562ee2a208a3110338
Ben Liblit <liblit@cs.wisc.edu>
2011-09-24 01:04:18 +0000

    Preferences dialog wrapper doesn't really do enough to warrant its own module.

--- launcher/tray/PreferencesDialog.py
+++
@@ -1,4 +0,0 @@
-def present():
-    from gi.repository import Gio
-    preferences = Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION, Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES | Gio.DBusProxyFlags.DO_NOT_CONNECT_SIGNALS, None, 'edu.wisc.cs.cbi.Preferences', '/edu/wisc/cs/cbi/Preferences', 'org.gtk.Application', None)
-    preferences.Activate('(a{sv})', None)

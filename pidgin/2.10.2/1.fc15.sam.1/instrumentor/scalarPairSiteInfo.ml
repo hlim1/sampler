2f91e16591749e97778a4d3e5e3c0c55bbbbf569
Ben Liblit <liblit@cs.wisc.edu>
2012-04-09 20:45:36 +0000

    pidgin-2.10.2-1.fc15.sam.1.i686

---
+++ pidgin/2.10.2/1.fc15.sam.1/instrumentor/scalarPairSiteInfo.ml
@@ -0,0 +1,38 @@
+open Cil
+open Pretty
+
+
+type left = lval * string * string
+type right = Constant of exp | Variable of varinfo * bool
+
+
+class c func inspiration left right =
+  object
+    inherit SiteInfo.c func inspiration as super
+
+    method! print =
+      let (leftLval, leftHost, leftOff) = left in
+      let leftLval = d_lval () leftLval in
+      let leftHost = text leftHost in
+      let leftOff = text leftOff in
+
+      let (rightExpr, rightKind) =
+	match right with
+	| Constant expr ->
+	    (d_exp () expr, "const")
+	| Variable (varinfo, mustInit) ->
+        let scope = if varinfo.vglob then
+                      "global"
+                    else if mustInit then
+                      "local-init"
+                    else
+                      "local-uninit"
+        in
+	    (text varinfo.vname, scope)
+      in
+      let rightKind = text rightKind in
+
+      super#print @
+      [leftLval; leftHost; leftOff;
+       rightExpr; rightKind]
+  end

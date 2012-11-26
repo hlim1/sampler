57065f7f1f4322c5c8d59bbff13d3f00098d8545
Ben Liblit <liblit@cs.wisc.edu>
2012-11-26 17:30:25 +0000

    gnumeric-1.11.6-1.fc17.sam.1.i686

---
+++ gnumeric/1.11.6/1.fc17.sam.1/lib/registry.h
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_sampler_registry_h
+#define INCLUDE_sampler_registry_h
+
+#include "signature.h"
+
+
+struct cbi_Unit {
+  struct cbi_Unit *next;
+  struct cbi_Unit *prev;
+  void (*reporter)(void);
+  void (*zeroSetter)(void);
+};
+
+
+void cbi_registerUnit(struct cbi_Unit *);
+void cbi_unregisterUnit(struct cbi_Unit *);
+
+void cbi_unregisterAllUnits(void);
+
+
+#endif /* !INCLUDE_sampler_registry_h */

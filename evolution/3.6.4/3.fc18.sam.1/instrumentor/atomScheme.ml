3836aeabb4c83491dbbb921e4cfad8331ee97f9f
Ben Liblit <liblit@cs.wisc.edu>
2013-04-10 16:41:50 +0000

    evolution-3.6.4-3.fc18.sam.1.i686

---
+++ evolution/3.6.4/3.fc18.sam.1/instrumentor/atomScheme.ml
@@ -0,0 +1,29 @@
+open SchemeName
+
+(*cci*)
+let name = {
+  flag = "atoms";
+  prefix = "atoms";
+  code = "A";
+  ident = "Atoms";
+}
+
+
+class c file : Scheme.c =
+  object
+    val tuples = new Counters.manager name file 
+
+    method private findAllSites =
+      let finder = new AtomFinder.visitor file tuples (* b_varinfo *) in
+      Scanners.iterFuncs file
+	(fun func ->
+	  let finder = finder func in
+	  ignore (Cil.visitCilFunction finder func  ));
+
+      tuples#patch  
+
+    method saveSiteInfo = tuples#saveSiteInfo 
+  end
+
+
+let factory = SchemeFactory.build name new c

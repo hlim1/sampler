9c7d35da660e0db1fd825d4e3e68b074ee67ea07
Ben Liblit <liblit@cs.wisc.edu>
2013-04-11 15:50:55 +0000

    Work around an LLVM version numbering glitch.

--- llvm-instrumentor/llvm.py
+++ llvm-instrumentor/llvm.py
@@ -5,6 +5,11 @@ from distutils.version import StrictVersion
 def check_llvm_version(context):
     context.Message('Checking for LLVM version...')
     success, output = context.TryAction('llvm-config --version >$TARGET')
+    output = output.rstrip()
+
+    # workaround for <http://llvm.org/bugs/show_bug.cgi?id=14715>
+    if output == '3.2svn': output = '3.2'
+
     output = StrictVersion(output.rstrip())
     context.Result(str(output))
     context.env['LLVM_version'] = output

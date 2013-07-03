b64761d8f195fcf177c877115fb779e27295870e
Ben Liblit <liblit@cs.wisc.edu>
2013-07-03 16:00:45 +0000

    sampler-1.9.3-1

---
+++ sampler/1.9.3/1/instrumentor/instrSiteInfo.ml
@@ -0,0 +1,11 @@
+open Cil
+
+
+(*cci*)
+class c func inspiration instruction =
+  object
+    inherit SiteInfo.c func inspiration as super
+
+    method! print =
+      super#print @ [dn_instr () instruction]
+  end

041cc3da8dccde3d1a0323b5a55759c434511a99
Ben Liblit <liblit@cs.wisc.edu>
2013-11-25 18:26:33 +0000

    gnumeric-1.12.8-1.fc18.sam.1.i686

---
+++ gnumeric/1.12.8/1.fc18.sam.1/instrumentor/exprSiteInfo.ml
@@ -0,0 +1,10 @@
+open Cil
+
+
+class c func inspiration expression =
+  object
+    inherit SiteInfo.c func inspiration as super
+
+    method! print =
+      super#print @ [d_exp () expression]
+  end

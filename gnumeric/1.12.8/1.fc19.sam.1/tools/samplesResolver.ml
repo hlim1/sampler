36428eb77885f0ba3ad9900b07559fee64218e96
Ben Liblit <liblit@cs.wisc.edu>
2013-11-25 16:42:33 +0000

    gnumeric-1.12.8-1.fc19.sam.1.x86_64

---
+++ gnumeric/1.12.8/1.fc19.sam.1/tools/samplesResolver.ml
@@ -0,0 +1,11 @@
+class loader registry =
+  object
+    inherit TaggedLoader.t "samples"
+
+    method private sectionHandler =
+      new SectionResolver.c registry
+  end
+
+
+let read registry =
+  (new loader registry)#readChannel

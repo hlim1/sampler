309a0eabfc32ac3d530ad7dd6d30b4695d391c90
Ben Liblit <liblit@cs.wisc.edu>
2014-03-18 16:44:34 +0000

    gnumeric-1.12.12-1.fc19.sam.1.x86_64

---
+++ gnumeric/1.12.12/1.fc19.sam.1/instrumentor/helpers.ml
@@ -0,0 +1,57 @@
+open Cil
+(* cci *)
+
+let is_bitfield lval =
+  let (_, offset) = lval in
+  let rec is_bitfield_offset_check offset=
+    match offset with
+      Field(finfo,offsetn) -> (finfo.fname = Cil.missingFieldName) ||
+                              (match finfo.fbitfield with Some(_) -> true | _-> false) ||
+                              is_bitfield_offset_check offsetn
+      | _-> false
+    in
+  is_bitfield_offset_check offset
+
+let scrub_filename str =
+  let new_str = ref "" in
+  String.iter 
+    (fun c -> match c 
+    with 
+      '.'| '/' | ':' | '-' -> new_str:= !new_str^"_" 
+    | _-> new_str := !new_str^(String.make 1 c) ) str;
+  !new_str
+  
+
+let get_prefix_file file =
+  ("cbi_"^scrub_filename(file.fileName))
+
+
+let findOrCreate_local func vname = 
+  try 
+  List.find ( fun vi -> vi.vname = vname) func.slocals 
+  with Not_found -> 
+    makeLocalVar func vname intType
+
+let findOrCreate_local_type func vname typ = 
+  try 
+  List.find ( fun vi -> vi.vname = vname) func.slocals 
+  with Not_found -> 
+    makeLocalVar func vname typ
+
+
+let create_global file vname =
+    let b_varinfo = makeGlobalVar vname intType in
+    let global  = GVar(b_varinfo, {init = Some (SingleInit zero)}, locUnknown) in 
+    file.globals <- global ::file.globals;
+    b_varinfo
+
+let findOrCreate_global file vname = 
+  try 
+    match(List.find 
+	    ( fun g -> match g with GVar(vi,_,_) -> vi.vname = vname  (*TODO check with GVarDecl too?*)
+	    | _-> false)
+	    file.globals) with
+    | GVar(vi,_,_) -> vi 
+    | _ -> create_global file vname
+  with Not_found ->  (*create global variable*)
+    create_global file vname

39ad130e90b88af8fd2c22f580afa9dda8f98063
Ben Liblit <liblit@cs.wisc.edu>
2014-06-02 20:25:32 +0000

    rhythmbox-3.0.3-1.fc20.sam.1.x86_64

---
+++ rhythmbox/3.0.3/1.fc20.sam.1/lib/schemes/coverage.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_sampler_coverage_report_h
+#define INCLUDE_sampler_coverage_report_h
+
+#include "../signature.h"
+#include "tuple-1.h"
+
+
+void cbi_coverageReport(const cbi_UnitSignature, unsigned, const cbi_Tuple1 []);
+
+
+#endif /* !INCLUDE_sampler_coverage_report_h */

3ae8c46dc15597aa3af76a50d55bdfff11272c1e
Ben Liblit <liblit@cs.wisc.edu>
2014-06-25 15:22:28 +0000

    gnumeric-1.12.17-1.fc20.sam.1.x86_64

---
+++ gnumeric/1.12.17/1.fc20.sam.1/instrumentor/embedSignature.ml
@@ -0,0 +1,11 @@
+open Cil
+
+
+let visit file digest =
+  try
+    let initinfo = FindGlobal.findInit "cbi_unitSignature" file in
+    let signature = Lazy.force digest in
+    let expr = mkString signature in
+    initinfo.init <- Some (SingleInit expr)
+  with
+    Missing.Missing _ -> ()

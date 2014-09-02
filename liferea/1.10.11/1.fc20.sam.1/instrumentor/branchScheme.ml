3ba54584c72f784f45ca85ff3e95127e45806c48
Ben Liblit <liblit@cs.wisc.edu>
2014-09-02 17:09:01 +0000

    liferea-1.10.11-1.fc20.sam.1.x86_64

---
+++ liferea/1.10.11/1.fc20.sam.1/instrumentor/branchScheme.ml
@@ -0,0 +1,28 @@
+open SchemeName
+
+
+let name = {
+  flag = "branches";
+  prefix = "branches";
+  code = "B";
+  ident = "Branches";
+}
+
+
+class c file : Scheme.c =
+  object
+    val tuples = new Counters.manager name file
+
+    method private findAllSites =
+      let finder = new BranchFinder.visitor file tuples in
+      Scanners.iterFuncs file
+	(fun func ->
+	  let finder = finder func in
+	  ignore (Cil.visitCilFunction finder func));
+      tuples#patch
+
+    method saveSiteInfo = tuples#saveSiteInfo
+  end
+
+
+let factory = SchemeFactory.build name new c

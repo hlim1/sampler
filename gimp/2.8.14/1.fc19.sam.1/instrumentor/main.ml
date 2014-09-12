eaded64d10a0d3eb12b65c56f8a7df46f7b5acbc
Ben Liblit <liblit@cs.wisc.edu>
2014-09-12 20:32:05 +0000

    gimp-2.8.14-1.fc19.sam.1.x86_64

---
+++ gimp/2.8.14/1.fc19.sam.1/instrumentor/main.ml
@@ -0,0 +1,58 @@
+open Cil
+
+
+let _ =
+
+  try
+    let phases =
+      [
+       "removing unused symbols (early)", (fun file -> Rmtmps.removeUnusedTemps file);
+       Locals.rename;
+       Instrumentor.phase;
+       "removing unused symbols (late)", (fun file -> Rmtmps.removeUnusedTemps file);
+       Idents.phase;
+       "printing transformed code", (dumpFile (new Printer.printer) stdout "")
+     ]
+    in
+
+    initCIL ();
+    lineDirectiveStyle := Some LinePreprocessorOutput;
+IFDEF HAVE_CASE_RANGE THEN
+    assert (not !useCaseRange)
+ELSE () END;
+IFDEF HAVE_COMPUTED_GOTO THEN
+    assert (not !useComputedGoto)
+ELSE () END;
+
+    (* back-ported from CIL; can be removed for CIL 1.4.1+ *)
+    let addSwap sizeInBits =
+      try
+	let name = Printf.sprintf "__builtin_bswap%d" sizeInBits in
+	if not (Hashtbl.mem builtinFunctions name) then
+	  begin
+	    assert (sizeInBits mod 8 = 0);
+	    let sizeInBytes = sizeInBits / 8 in
+	    let sizedIntType = TInt (intKindForSize sizeInBytes false, []) in
+	    Hashtbl.add builtinFunctions name (sizedIntType, [ sizedIntType ], false)
+	  end
+      with Not_found ->
+	()
+    in
+    addSwap 32;
+    addSwap 64;
+
+    (* back-ported from CIL; can be removed for CIL 1.4.1+ *)
+    List.iter (fun attr -> Hashtbl.add attributeHash attr (AttrFunType false))
+      [
+       "artificial";
+       "leaf";
+       "nonnull";
+       "warn_unused_result";
+     ];
+
+    Arg.parse (Options.argspecs ())
+      (TestHarness.doOne phases)
+      ("Usage:" ^ Sys.executable_name ^ " [<flag> | <source>] ...")
+
+  with Errormsg.Error ->
+    exit 2

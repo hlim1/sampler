25f55ebffd1301e274adc4d4248d289b36d8efa0
Ben Liblit <liblit@cs.wisc.edu>
2014-12-09 22:24:29 +0000

    pidgin-2.10.10-3.fc20.sam.1.x86_64

---
+++ pidgin/2.10.10/3.fc20.sam.1/launcher/wrapper/Main2.py
@@ -0,0 +1,41 @@
+from os.path import abspath, dirname, join
+from sys import path, stderr
+
+path.insert(1, join(dirname(dirname(abspath(__file__))), 'common'))
+
+
+########################################################################
+
+
+def main(name, wrapped, upload_headers, **extras):
+    __pychecker__ = 'no-argsused'
+    from gi.repository import Gio
+    from AppConfig import AppConfig
+    import Keys
+
+    app = AppConfig(name, wrapped, upload_headers)
+    settings = Gio.Settings(Keys.BASE)
+
+    if settings[Keys.ASKED] and settings[Keys.MASTER]:
+        import SampledLauncher
+        sparsity = 100
+        reporting_url = 'https://cbi.cs.wisc.edu/cbi/cgi-bin/sampler-upload.cgi'
+        launcher = SampledLauncher.SampledLauncher(settings, app, sparsity, reporting_url)
+    else:
+        import UnsampledLauncher
+        launcher = UnsampledLauncher.UnsampledLauncher(app)
+    launcher.spawn()
+
+    if not settings[Keys.ASKED]:
+        try:
+            Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION, Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES | Gio.DBusProxyFlags.DO_NOT_CONNECT_SIGNALS, None, 'edu.wisc.cs.cbi.FirstTime', '/edu/wisc/cs/cbi/FirstTime', 'org.gtk.Application', None).Activate('(a{sv})', None)
+        except RuntimeError, error:
+            print >>stderr, "warning: cannot activate CBI first-time dialog:", error
+
+    try:
+        Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION, Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES | Gio.DBusProxyFlags.DO_NOT_CONNECT_SIGNALS, None, 'edu.wisc.cs.cbi.Monitor', '/edu/wisc/cs/cbi/Monitor', 'edu.wisc.cs.cbi.Monitor', None).activate()
+    except RuntimeError, error:
+        print >>stderr, "warning: cannot activate CBI tray icon:", error
+
+    outcome = launcher.wait()
+    outcome.exit()
